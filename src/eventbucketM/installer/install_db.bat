02e90152469d9c6cc982be0f193f91817a514521
sh <16303791+speedyhoon@users.noreply.github.com>
2015-03-01 11:00:01 +1100

    Massive Project cleanup Delete

--- src/eventbucketM/installer/install_db.bat
+++
@@ -1,1 +0,0 @@
-ebd.exe --serviceDescription "Provides fast local database storage for EventBucket. If this service is stopped, the local EventBucket HTTP server will fail to operate." --serviceDisplayName "EventBucket Storage" --serviceName EventBucketDB --install --logpath .\log --port 38888 --nohttpinterface --noscripting --smallfiles --nssize 1
--- src/eventbucketM/old_backup_files/Compulary_backup/run.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Users\Developer\EBrepo\src\eventbucketM
-go run main.go database.go p_home.go schema.go settings.go template.go utils.go
