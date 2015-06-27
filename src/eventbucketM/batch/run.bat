56ebb25b6b4081a3c21377576d065ea92588764a
Cam <eventbucket@hotmail.com>
2015-06-27 10:21:09 +1000

    Start buildDate and EB with race detection in verbose mode

--- src/eventbucketM/batch/run.bat
+++ src/eventbucketM/batch/run.bat
@@ -3,10 +3,10 @@ set go_files=dev.go database.go form.go httpGzip.go pAbout.go pArchive.go pClub.
 cd %root_dir%\eventbucketM\golang
 gofmt -s=true -e=true -w=true -l=true %go_files%
 cd %root_dir%\go_utils
-go run buildDate.go
+go run -race -v buildDate.go
 cd %root_dir%\eventbucketM\!
 rem gofmt -comments=false -s=true -e=true -w=true %go_files%
 gofmt -s=true -e=true -w=true %go_files%
 go tool vet -shadowstrict -test ./
 gometalinter --concurrency=2 --cyclo-over="60" --sort=path
-go run %go_files%
+go run -race -v %go_files%
