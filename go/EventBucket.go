30a4652a2658754ac9e5d9faf07f2562739b3591
sh <16303791+speedyhoon@users.noreply.github.com>
2015-10-23 08:46:31 +1100

    new robots file, add serveFile and gzipContents, tidy up code formatting.

--- go/EventBucket.go
+++ go/EventBucket.go
@@ -7,13 +7,13 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
-	"time"
 	"path/filepath"
-//	"fmt"
+	"time"
+	//	"fmt"
 )
 
 const (
-	debug   = true
+	debug = true
 
 	//Logging & Database directory name
 	subDir = "/EventBucket"
@@ -22,8 +22,11 @@ const (
 	address = "http://localhost"
 	port    = "80"
 	dirRoot = "./"
-//	dirCSS  = "/css/"
-	dirJS   = "/js/"
+	//	dirCSS  = "/css/"
+	dirJS     = "/js/"
+	robots = "robots.txt"
+	favicon = "favicon.ico"
+	sitemap = "sitemap.xml"
 	//dirHTML = "/html/"
 	//dirSVG  = "/svg/"
 	//dirWOF2 = "/woff2/"
@@ -34,8 +37,8 @@ var (
 	expiresTime string
 
 	//Logging
-	tempPath = os.Getenv("temp") + subDir
-	logFileName = filepath.Join(tempPath, time.Now().Format("20060102") + ".log")
+	tempPath    = os.Getenv("temp") + subDir
+	logFileName = filepath.Join(tempPath, time.Now().Format("20060102")+".log")
 	//Logging destinations are os.Stdout, os.Stderr, ioutil.Discard
 	trace   = log.New(os.Stdout, "TRACE:   ", log.Lshortfile)
 	info    = log.New(os.Stdout, "INFO:    ", log.Lshortfile|log.Ltime)
@@ -45,7 +48,7 @@ var (
 	databasePath = os.Getenv("ProgramData") + subDir
 )
 
-func init(){
+func init() {
 	startLogging()
 	go maintainExpiresTime()
 	go mkDir(databasePath)
@@ -53,20 +56,24 @@ func init(){
 }
 
 func main() {
+	serveFile(sitemap)
+	serveFile(favicon)
+	serveFile(robots)
 	serveDir(dirJS)
-	if !debug && exec.Command(`rundll32.exe`, "url.dll,FileProtocolHandler", address).Start() != nil {
+
+	if !debug && exec.Command("rundll32.exe", "url.dll,FileProtocolHandler", address).Start() != nil {
 		warning.Print("Unable to open a web browser for " + address)
 	}
 	info.Print("Starting EventBucket server...")
-	warning.Printf("ListenAndServe: %v", http.ListenAndServe(":" + port, nil))
+	warning.Printf("ListenAndServe: %v", http.ListenAndServe(":"+port, nil))
 }
 
 //Setup logging into temp directory
-func startLogging(){
+func startLogging() {
 	err := mkDir(tempPath)
 	if err == nil {
 		var f *os.File
-		f, err =  os.OpenFile(logFileName,  os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		f, err = os.OpenFile(logFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 		if err == nil {
 			trace.SetOutput(f)
 			info.SetOutput(f)
@@ -77,7 +84,7 @@ func startLogging(){
 }
 
 // Attempt to create the path supplied if it doesn't exist.
-func mkDir(path string)error{
+func mkDir(path string) error {
 	stat, err := os.Stat(path)
 	if err != nil || !stat.IsDir() {
 		err = os.Mkdir(path, os.ModeDir)
@@ -89,7 +96,7 @@ func mkDir(path string)error{
 }
 
 //Update the expires http header time, every 15 minutes rather than recalculating it on every http request.
-func maintainExpiresTime(){
+func maintainExpiresTime() {
 	ticker := time.NewTicker(time.Minute * 15)
 	for range ticker.C {
 		//Can't directly change global variables in a go routine, so call an external function.
@@ -98,9 +105,8 @@ func maintainExpiresTime(){
 }
 
 //Set expiry date 1 year, 0 months & 0 days in the future.
-func setExpiresTime(){
+func setExpiresTime() {
 	//Date format is the same as Go`s time.RFC1123 but uses "GMT" timezone instead of "UTC" time standard.
 	expiresTime = time.Now().UTC().AddDate(1, 0, 0).Format("Mon, 02 Jan 2006 15:04:05 GMT")
 	//w3.org says: "All HTTP date/time stamps MUST be represented in Greenwich Mean Time" under 3.3.1 Full Date //www.w3.org/Protocols/rfc2616/rfc2616-sec3.html
-	trace.Print("Updated expires time: "+ expiresTime)
 }
--- go/http.go
+++ go/http.go
@@ -5,6 +5,27 @@ import (
 	"strings"
 )
 
+func serveFile(fileName string) {
+	http.HandleFunc("/" + fileName, func(w http.ResponseWriter, r *http.Request) {
+
+			httpHeaders(w, []string{cache})
+			gzipContents(w, r,
+				func(){
+					http.ServeFile(w, r, "./gz/"+fileName)
+				},
+				func(){
+					http.ServeFile(w, r, "./"+fileName)
+				})
+
+//			if strings.Contains(r.Header.Get(acceptEncoding), "gzip") {
+//				http.ServeFile(w, r, "./gz/"+fileName)
+//			}else {
+//				http.ServeFile(w, r, "./"+fileName)
+//				trace.Print("served " + fileName + " with NO compression")
+//			}
+		})
+}
+
 func serveDir(contentType string) {
 	http.Handle(contentType,
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -14,25 +35,54 @@ func serveDir(contentType string) {
 				return
 			}
 
-			if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-				warning.Print("The current browser does not support gzip")
-			}
-			w.Header().Set("Content-Encoding", "gzip")
-
-			//			w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")	//Don't cache any response
+			//			w.Header().Set(cacheControl, "no-cache, no-store, must-revalidate")	//Don't cache any response
 			//			w.Header().Set("Pragma", "no-cache")
 			//			w.Header().Set("Expires", "0")	//Expires now
 			//			w.Header().Set("Content-Encoding", "gzip")
 			//			Gzip(http.FileServer(http.Dir(DIR_ROOT)), w, r)
 			//			http.FileServer(http.Dir("./gz/")).ServeHTTP(w, r)
-			if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-				w.Header().Set("Content-Encoding", "gzip")
-				http.StripPrefix(contentType, http.FileServer(http.Dir("./gz/"))).ServeHTTP(w, r)
-			} else {
-				http.FileServer(http.Dir(dirRoot)).ServeHTTP(w, r)
-			}
+			httpHeaders(w, []string{cache})
+
+			gzipContents(w, r,
+				func(){
+					http.StripPrefix(contentType, http.FileServer(http.Dir("./gz/"))).ServeHTTP(w, r)
+				},
+				func(){
+					http.FileServer(http.Dir(dirRoot)).ServeHTTP(w, r)
+				})
+
+
+//			if strings.Contains(r.Header.Get(acceptEncoding), gzip) {
+//				httpHeaders(w, []string{gzip})
+//				http.StripPrefix(contentType, http.FileServer(http.Dir("./gz/"))).ServeHTTP(w, r)
+//			} else {
+//				http.FileServer(http.Dir(dirRoot)).ServeHTTP(w, r)
+//				warning.Print("The current browser does not support gzip")
+//			}
 		}))
 }
+
+func gzipContents(w http.ResponseWriter, r *http.Request, serveNormal, serveGzip func()){
+	if strings.Contains(r.Header.Get(acceptEncoding), gzip) {
+		httpHeaders(w, []string{gzip})
+		serveGzip()
+	} else {
+		serveNormal()
+		warning.Print("The current browser does not support gzip")
+	}
+}
+
+
+
+const (
+	contentType = "Content-Type"
+	cacheControl = "Cache-Control"
+	expires = "Expires"
+	cache   = "cache"
+	nocache = "nocache"
+	gzip = "gzip"
+	acceptEncoding = "Accept-Encoding"
+)
 
 func httpHeaders(w http.ResponseWriter, setHeaders []string) {
 	//w.Header().Set("Content-Security-Policy", "default-src 'none'; style-src 'self'; script-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self'; font-src 'self'")
@@ -42,25 +92,39 @@ func httpHeaders(w http.ResponseWriter, setHeaders []string) {
 	w.Header().Set("X-Frame-Options", "DENY")
 	headers := map[string][2]string{
 		//Cache
-		"public":    {"Cache-Control", "public"},
-		"expire":    {"Expires", expiresTime},
-		"cache":     {"Vary", "Accept-Encoding"},
+//		"public":    {cacheControl, "public"},
+//		"expire":    {expires, expiresTime},
+//		"cache":     {"Vary", acceptEncoding},
 
 		//Don't cache
-		"noCache":   {"Cache-Control", "no-cache, no-store, must-revalidate"},
-		"expireNow": {"Expires", "0"},
-		"pragma":    {"Pragma", "no-cache"},
+//		"noCache":   {cacheControl, "no-cache, no-store, must-revalidate"},
+//		"expireNow": {expires, "0"},
+//		"pragma":    {"Pragma", "no-cache"},
 
-		"html":      {"Content-Type", "text/html; charset=utf-8"},
-		dirJS:       {"Content-Type", "text/javascript"},
-		//dirCSS:      {"Content-Type", "text/css; charset=utf-8"},
-		//dirSVG:      {"Content-Type", "image/svg+xml"},
-		//dirWOF2:     {"Content-Type", "application/font-woff2"},
-		//dirPNG:      {"Content-Type", "image/png"},
-		//dirJPEG:     {"Content-Type", "image/jpeg"},
-		//dirWOF:      {"Content-Type", "application/font-woff"},
+		gzip:      {"Content-Encoding", "gzip"},
+		"html":      {contentType, "text/html; charset=utf-8"},
+		dirJS:       {contentType, "text/javascript"},
+		//dirCSS:      {contentType, "text/css; charset=utf-8"},
+		//dirSVG:      {contentType, "image/svg+xml"},
+		//dirWOF2:     {contentType, "application/font-woff2"},
+		//dirPNG:      {contentType, "image/png"},
+		//dirJPEG:     {contentType, "image/jpeg"},
+		//dirWOF:      {contentType, "application/font-woff"},
 	}
 	for _, lookup := range setHeaders {
-		w.Header().Set(headers[lookup][0], headers[lookup][1])
+		switch lookup{
+		case cache:
+			w.Header().Set(cacheControl, "public")
+			w.Header().Set(expires, expiresTime)
+			w.Header().Set("Vary", acceptEncoding)
+			break
+		case nocache:
+			w.Header().Set(cacheControl, "no-cache, no-store, must-revalidate")
+			w.Header().Set(expires, "0")
+			w.Header().Set("Pragma", "no-cache")
+			break
+		default:
+			w.Header().Set(headers[lookup][0], headers[lookup][1])
+		}
 	}
 }
