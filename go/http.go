a776d1cc90d1e6b6e438b79900fb77a89af209b2
Cam <eventbucket@hotmail.com>
2015-10-23 08:51:56 +1100

    remove comments & rename functions & variables

--- go/http.go
+++ go/http.go
@@ -7,22 +7,14 @@ import (
 
 func serveFile(fileName string) {
 	http.HandleFunc("/" + fileName, func(w http.ResponseWriter, r *http.Request) {
-
-			httpHeaders(w, []string{cache})
-			gzipContents(w, r,
+			headers(w, []string{cache})
+			serveGzip(w, r,
 				func(){
 					http.ServeFile(w, r, "./gz/"+fileName)
 				},
 				func(){
 					http.ServeFile(w, r, "./"+fileName)
 				})
-
-//			if strings.Contains(r.Header.Get(acceptEncoding), "gzip") {
-//				http.ServeFile(w, r, "./gz/"+fileName)
-//			}else {
-//				http.ServeFile(w, r, "./"+fileName)
-//				trace.Print("served " + fileName + " with NO compression")
-//			}
 		})
 }
 
@@ -34,45 +26,26 @@ func serveDir(contentType string) {
 				http.NotFound(w, r)
 				return
 			}
-
-			//			w.Header().Set(cacheControl, "no-cache, no-store, must-revalidate")	//Don't cache any response
-			//			w.Header().Set("Pragma", "no-cache")
-			//			w.Header().Set("Expires", "0")	//Expires now
-			//			w.Header().Set("Content-Encoding", "gzip")
-			//			Gzip(http.FileServer(http.Dir(DIR_ROOT)), w, r)
-			//			http.FileServer(http.Dir("./gz/")).ServeHTTP(w, r)
-			httpHeaders(w, []string{cache})
-
-			gzipContents(w, r,
+			headers(w, []string{cache})
+			serveGzip(w, r,
 				func(){
 					http.StripPrefix(contentType, http.FileServer(http.Dir("./gz/"))).ServeHTTP(w, r)
 				},
 				func(){
 					http.FileServer(http.Dir(dirRoot)).ServeHTTP(w, r)
 				})
-
-
-//			if strings.Contains(r.Header.Get(acceptEncoding), gzip) {
-//				httpHeaders(w, []string{gzip})
-//				http.StripPrefix(contentType, http.FileServer(http.Dir("./gz/"))).ServeHTTP(w, r)
-//			} else {
-//				http.FileServer(http.Dir(dirRoot)).ServeHTTP(w, r)
-//				warning.Print("The current browser does not support gzip")
-//			}
 		}))
 }
 
-func gzipContents(w http.ResponseWriter, r *http.Request, serveNormal, serveGzip func()){
+func serveGzip(w http.ResponseWriter, r *http.Request, ungzipped, gzipped func()){
 	if strings.Contains(r.Header.Get(acceptEncoding), gzip) {
-		httpHeaders(w, []string{gzip})
-		serveGzip()
+		headers(w, []string{gzip})
+		gzipped()
 	} else {
-		serveNormal()
+		ungzipped()
 		warning.Print("The current browser does not support gzip")
 	}
 }
-
-
 
 const (
 	contentType = "Content-Type"
@@ -84,23 +57,13 @@ const (
 	acceptEncoding = "Accept-Encoding"
 )
 
-func httpHeaders(w http.ResponseWriter, setHeaders []string) {
+func headers(w http.ResponseWriter, setHeaders []string) {
 	//w.Header().Set("Content-Security-Policy", "default-src 'none'; style-src 'self'; script-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self'; font-src 'self'")
 	w.Header().Set("Content-Security-Policy", "default-src 'none'")
 
 	//The page cannot be displayed in a frame, regardless of the site attempting to do so. //developer.mozilla.org/en-US/docs/Web/HTTP/X-Frame-Options
 	w.Header().Set("X-Frame-Options", "DENY")
 	headers := map[string][2]string{
-		//Cache
-//		"public":    {cacheControl, "public"},
-//		"expire":    {expires, expiresTime},
-//		"cache":     {"Vary", acceptEncoding},
-
-		//Don't cache
-//		"noCache":   {cacheControl, "no-cache, no-store, must-revalidate"},
-//		"expireNow": {expires, "0"},
-//		"pragma":    {"Pragma", "no-cache"},
-
 		gzip:      {"Content-Encoding", "gzip"},
 		"html":      {contentType, "text/html; charset=utf-8"},
 		dirJS:       {contentType, "text/javascript"},
