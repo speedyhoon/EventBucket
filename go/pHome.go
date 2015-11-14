5727bbda902a06a90049ae41c28dd4f80fa221be
sh <16303791+speedyhoon@users.noreply.github.com>
2015-11-14 13:17:40 +1100

    Add simple multi layer html template

--- go/pHome.go
+++ go/pHome.go
@@ -4,8 +4,8 @@ import "net/http"
 
 func home(w http.ResponseWriter, r *http.Request) {
 	templater(Page{
-		title: "Home",
-		data: M{
+		Title: "Home",
+		Data: M{
 			"Stuff": "Hommmer page!",
 		},
 	}, w)
@@ -13,9 +13,63 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func event(w http.ResponseWriter, r *http.Request) {
 	templater(Page{
-		title: "Event",
-		data: M{
+		Title: "Event",
+		Data: M{
 			"Stuff": "EVENT page!",
+		},
+	}, w)
+}
+
+func events(w http.ResponseWriter, r *http.Request) {
+	templater(Page{
+		Title: "Events",
+		Data: M{
+			"Stuff": "EVENTS page!",
+		},
+	}, w)
+}
+
+func eventArchive(w http.ResponseWriter, r *http.Request) {
+	templater(Page{
+		Title: "Archive",
+		Data: M{
+			"Stuff": "Archive page!",
+		},
+	}, w)
+}
+
+func clubs(w http.ResponseWriter, r *http.Request) {
+	templater(Page{
+		Title: "Clubs",
+		Data: M{
+			"Stuff": "CLUBS page!",
+		},
+	}, w)
+}
+
+func shooters(w http.ResponseWriter, r *http.Request) {
+	templater(Page{
+		Title: "Shooters",
+		Data: M{
+			"Stuff": "SHOOTERS page!",
+		},
+	}, w)
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	templater(Page{
+		Title: "About",
+		Data: M{
+			"Stuff": "About page!",
+		},
+	}, w)
+}
+
+func licence(w http.ResponseWriter, r *http.Request) {
+	templater(Page{
+		Title: "Licence",
+		Data: M{
+			"Stuff": "Licence page!",
 		},
 	}, w)
 }
