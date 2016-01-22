b71f335d8a92ee00fbce265e70ba086131bae3de
sh <16303791+speedyhoon@users.noreply.github.com>
2016-01-23 09:50:09 +1100

    improve sessions so they work. auto session expiry not implemented yet.

--- go/pEvent.go
+++ go/pEvent.go
@@ -1,10 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"time"
-)
+import "net/http"
 
 func event(w http.ResponseWriter, r *http.Request, eventId string) {
 	//	eventID := strings.TrimPrefix(r.URL.Path, urlEvent)
@@ -15,13 +11,10 @@ func event(w http.ResponseWriter, r *http.Request, eventId string) {
 	if cookie != "" {
 
 	}
-
 	if r.URL.Path[len(urlEvent):] == "3C" {
 		errorHandler(w, r, http.StatusNotFound)
 		return
 	}
-
-	//	eventId = "1A"
 	templater(w, page{
 		Title: "Event",
 		Data: M{
@@ -49,58 +42,17 @@ func insertEvent(w http.ResponseWriter, r *http.Request) {
 		using GET on a form which requires data to be presented via POST, or using POST on a read-only resource.
 		//en.wikipedia.org/wiki/List_of_HTTP_status_codes*/
 
-		//		globalSessions["fdsa"] = sessionInfo{inputs: []input{
-		//			{Error: "0"},
-		//			{Error: "1"},
-		//			{Error: "2"},
-		//			{Error: "3"},
-		//		},
-		//			expiry: time.Now(),
-		//		}
-		//		w.Header().Add("Set-Cookie", "z=fdsa; expires="+time.Now().Add(2*time.Minute).Format(gmtFormat))
-
-		//		globalSessions["fdsa"] = sessionInfo{
-		//			form: form{
-		//				inputs: []input{
-		//					{Error: "0"},
-		//					{Required: true, Error: "1"},
-		//					{Value: time.Now().Format("2006-01-02"), Error: "2"},
-		//					{Value: time.Now().Format("15:04"), Error: "3"},
-		//				},
-		//			},
-		//		}
-
-		//		http.Redirect(w, r, "/", http.StatusSeeOther)
 		http.Redirect(w, r, "/", http.StatusMethodNotAllowed)
-
-		//redirect to home page
-		//				errorHandler(w, r, http.StatusMethodNotAllowed)
 		return
 	}
 	submittedFields, isValid := isValid(r, GlobalForms[0].fields)
 	if !isValid {
-		//		info.Println(err)
-
-		sesId := newSessionID()
-		expiry := time.Now().Add(2 * time.Minute)
-
-		w.Header().Add("Set-Cookie", sessionError("invalid number of form items"))
-		//
-		//		http.Redirect(w, r, "/", http.StatusSeeOther)
-		//		return
-
-		globalSessions[sesId] = sessionInfo{
-			expiry: expiry,
-			form: form{
-				action: "0",
-				fields: submittedFields,
-			},
-		}
-		w.Header().Add("Set-Cookie", fmt.Sprintf("z=%v; expires=%v", sesId, expiry.Format(gmtFormat)))
+		setSession(w, form{
+			action: "0",
+			fields: submittedFields,
+		})
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
-
 	http.Redirect(w, r, "/event/1a", http.StatusSeeOther)
-
 }
--- go/session.go
+++ go/session.go
@@ -8,72 +8,13 @@ import (
 )
 
 const (
-	sessionIDLength  = 24 //Recommended to be at least 16 characters long.
-	semicolon        = 59
-	sessionCharStart = 33
-	sessionCharRange = 126 - sessionCharStart
+	sessionToken      = "s"
+	sessionIDLength   = 24 //Recommended to be at least 16 characters long.
+	sessionCharStart  = 33
+	sessionCharRange  = 126 - sessionCharStart
+	semicolon         = 59
+	sessionExpiryTime = 2 * time.Minute
 )
-
-//TODO leave only one of these global variables
-var sessionForm = make(map[string]form, 1)
-var globalSessions = make(map[string]sessionInfo, 1)
-
-//TODO will possibly need a chanel here to prevent locks occurring
-func setSession(returns form) {
-	var number int
-	var sessionID string
-	var ok bool
-	//generate a new session id while it doesn't already exist
-	for _, ok = sessionForm[sessionID]; sessionID != "" && !ok; {
-		//clear any previous id that was generated
-		sessionID = ""
-		for len(sessionID) < sessionIDLength {
-			number = rand.Intn(sessionCharRange) + sessionCharStart
-			//ignore semicolons ";" as these characters terminate the end of the session id
-			if number != semicolon {
-				//converts number to a alphanumeric or symbol character
-				sessionID += string(number)
-			}
-		}
-	}
-	sessionForm[sessionID] = returns
-}
-
-//When a previous session id is used remove it.
-//func getSession(id string) (form, error) {
-func getSession(id string) form {
-	contents, ok := sessionForm[id]
-	if ok {
-		//Clear the session contents as it has been returned to the user.
-		delete(sessionForm, id)
-		//		return contents, nil
-		return contents
-	}
-	//	return form{}, errors.New("Couldn't find a session with id " + id)
-	return form{}
-}
-
-//SessionID's can't have space or semicolon
-//should be 16 characters
-//stackoverflow.com/questions/1969232/allowed-characters-in-cookies
-func newSessionID() string {
-	var newSessionId string
-	var i, randInt int
-	for i < 24 {
-		randInt = 33 + rand.Intn(93)
-		if randInt != 59 { //ignore semicolons ;
-			i++
-			newSessionId += string(randInt)
-		}
-	}
-	return "z=" + newSessionId
-}
-
-func sessionError(error string) string {
-	sessionID := newSessionID()
-	//	globalSessions[sessionID] = error
-	return sessionID + "; Expires=" + time.Now().Add(1*time.Minute).Format("Mon, Jan 2 2006 15:04:05 GMT")
-}
 
 type sessionInfo struct {
 	inputs []field
@@ -81,14 +22,58 @@ type sessionInfo struct {
 	expiry time.Time
 }
 
+var globalSessions = make(map[string]sessionInfo, 1)
+
+//TODO will possibly need a chanel here to prevent locks occurring
+func setSession(w http.ResponseWriter, returns form) {
+	var number int
+	var sessionID string
+	//var ok bool
+	//generate a new session id while it doesn't already exist
+	//for _, ok = globalSessions[sessionID]; sessionID != "" && !ok; {
+	//clear any previous id that was generated
+	sessionID = ""
+	//TODO could possibly use cryptography random letter generator
+	for len(sessionID) < sessionIDLength {
+		number = rand.Intn(sessionCharRange) + sessionCharStart
+		//ignore semicolons ";" as these characters terminate the end of the session id
+		if number != semicolon {
+			//converts number to a alphanumeric or symbol character
+			sessionID += string(number)
+		}
+	}
+	//	}
+	info.Println(sessionID)
+	expiry := time.Now().Add(sessionExpiryTime)
+	globalSessions[sessionID] = sessionInfo{
+		expiry: expiry,
+		form:   returns,
+	}
+	w.Header().Add("Set-Cookie", fmt.Sprintf("%v=%v; expires=%v", sessionToken, sessionID, expiry.Format(formatGMT)))
+}
+
+//SessionID's should be at least 16 characters length can't have space or semicolon
+//stackoverflow.com/questions/1969232/allowed-characters-in-cookies
+func newSessionID() string {
+	var sessionId string
+	var i, randInt int
+	for i < 24 {
+		randInt = 33 + rand.Intn(93)
+		if randInt != 59 { //ignore semicolons ;
+			i++
+			sessionId += string(randInt)
+		}
+	}
+	return sessionToken + "=" + sessionId
+}
+
 /* TODO
 create a ticker that checks the saved sessions every 90 seconds. If the session is older than 1 minute, delete it.
 */
 
-const cookieToken = "s"
-
-func getSessionForm2(w http.ResponseWriter, r *http.Request) form {
-	cookies, err := r.Cookie("z")
+//When a session id is used remove it.
+func getSession(w http.ResponseWriter, r *http.Request) form {
+	cookies, err := r.Cookie(sessionToken)
 	if err != nil || cookies.Value == "" {
 		return form{}
 	}
@@ -97,35 +82,8 @@ func getSessionForm2(w http.ResponseWriter, r *http.Request) form {
 	if ok {
 		//Clear the session contents as it has been returned to the user.
 		delete(globalSessions, cookies.Value)
-		//		return contents, nil
-		w.Header().Set("Set-Cookie", fmt.Sprintf("z=; expires=%v", time.Now().UTC().Add(-5*time.Minute).Format(gmtFormat)))
+		w.Header().Set("Set-Cookie", fmt.Sprintf("%v=; expires=%v", sessionToken, time.Now().UTC().Add(-sessionExpiryTime).Format(formatGMT)))
 		return contents.form
 	}
-	//	return form{}, errors.New("Couldn't find a session with id " + id)
 	return form{}
-}
-
-func sessionExpire(w http.ResponseWriter, r *http.Request) {
-	//	var myForm []field
-	//	info.Println("globalSessions", globalSessions)
-	cookies, err := r.Cookie("z")
-	info.Println("r.Cookie=", cookies, err)
-
-	//	w.Header().Del("z=fdsa")
-
-	//	w.Header().Set("Set-Cookie", fmt.Sprintf("z=; path=/; expires=%v", time.Now().UTC().Add(-5*time.Minute).Format(gmtFormat)))
-	w.Header().Set("Set-Cookie", fmt.Sprintf("z=; expires=%v", time.Now().UTC().Add(-5*time.Minute).Format(gmtFormat)))
-}
-
-func newSessionForm(w http.ResponseWriter, r *http.Request) {
-	globalSessions["fdsa"] = sessionInfo{
-		expiry: time.Now(),
-		form: form{fields: []field{
-			{Error: "-----0"},
-			{Error: "-----1"},
-			{Error: "-----2"},
-			{Error: "-----3"},
-		}},
-	}
-	w.Header().Add("Set-Cookie", "z=fdsa; expires="+time.Now().Add(2*time.Minute).Format(gmtFormat))
 }
