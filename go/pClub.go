f158bfa8773f0d2e20319009bb7d145f285c5f76
sh <16303791+speedyhoon@users.noreply.github.com>
2016-01-24 13:03:09 +1100

    Fix About img paths Add club page Add temp menu item links

---
+++ go/pClub.go
@@ -0,0 +1,64 @@
+package main
+
+import "net/http"
+
+func club(w http.ResponseWriter, r *http.Request, clubID string) {
+	//	sessionForm := getSession(w, r)
+	//	sessionFields := sessionForm.fields
+
+	templater(w, page{
+		Title: "Club",
+		//		Data: M{
+		//			"NewClub": sessionFields,
+		/*[]field{
+				{
+					Error:   "hey hey!",
+					Value:   "true",
+					Options: []option{},
+				}, {
+					Error: "I caused an error!@",
+					Value: "fds",
+					Options: []option{
+						{Label: "label", Value: "2 3"},
+						{Label: "text", Value: `"T`},
+						{Label: "search", Value: ">S"},
+					},
+				}, {
+					Value: "AbC",
+				},
+			},
+		}*/
+		//		},
+		///
+	})
+}
+
+func clubs(w http.ResponseWriter, r *http.Request) {
+	sessionForm := getSession(w, r)
+	sessionFields := sessionForm.fields
+
+	templater(w, page{
+		Title: "Clubs",
+		Data: M{
+			"NewClub": sessionFields,
+			/*[]field{
+					{
+						Error:   "hey hey!",
+						Value:   "true",
+						Options: []option{},
+					}, {
+						Error: "I caused an error!@",
+						Value: "fds",
+						Options: []option{
+							{Label: "label", Value: "2 3"},
+							{Label: "text", Value: `"T`},
+							{Label: "search", Value: ">S"},
+						},
+					}, {
+						Value: "AbC",
+					},
+				},
+			}*/
+		},
+	})
+}
