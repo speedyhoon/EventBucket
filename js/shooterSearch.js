fef44a873a707ae65393cf989b9a2205a056ed1c
sh <16303791+speedyhoon@users.noreply.github.com>
2016-02-26 08:29:01 +1100

    Add loading spinner & styling Add ajax error red style Finish JS code to automatically retrieve shooters when first name, surname, or club changes

--- js/shooterSearch.js
+++ js/shooterSearch.js
@@ -1,9 +1,23 @@
 'use strict';
-var xhttp = new XMLHttpRequest;
-xhttp.onreadystatechange = function() {
-  if (xhttp.readyState == 4 && xhttp.status == 200) {
-    console.log(xhttp.responseText);
-  }
-};
-xhttp.open('GET', '/9?f=Fred&s=Smith&C=Treeville', true);
-xhttp.send();
+var f = '', s = '', c = '';
+function getter(form) {
+  //Ignore form inputs into other fields
+  if (f === form.f.value && s === form.s.value && c === form.C.value) return;
+  f = form.f.value;
+  s = form.s.value;
+  c = form.C.value;
+  var xhttp = new XMLHttpRequest;
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4) {
+      if (xhttp.status == 200) {
+        form.S.innerHTML = xhttp.responseText;
+        form.S.removeAttribute('class');
+      }else {
+        form.S.setAttribute('class', 'not200');
+      }
+    }
+  };
+  form.S.setAttribute('class', 'loading');
+  xhttp.open('GET', '/9?f=' + f + '&s=' + s + '&C=' + c, true);
+  xhttp.send();
+}
