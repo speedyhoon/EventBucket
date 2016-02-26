68e84e727df8a858c9fa3ec1dfead9b6bbeaad5a
Cam <eventbucket@hotmail.com>
2016-02-27 09:18:11 +1100

    remove commented out code

--- go/pClubSettings.go
+++ go/pClubSettings.go
@@ -18,34 +18,9 @@ func clubSettings(w http.ResponseWriter, r *http.Request, clubID string) {
 	detailsForm.Fields[4].Value = trimFloat(club.Latitude)
 	detailsForm.Fields[5].Value = trimFloat(club.Longitude)
 	detailsForm.Fields[6].Value = toB36(club.ID)
+
 	newMoundForm := getFormSession(w, r, clubMoundNew)
 	newMoundForm.Fields[0].Value = toB36(club.ID)
-
-	/*
-		var invalidForm, detailsForm, newMoundForm form
-		invalidForm = getSession(w, r, []uint8{clubDetails})
-		if invalidForm.action == clubDetails {
-			detailsForm = invalidForm
-		} else {
-			detailsForm = form{Fields: []field{
-				{Value: club.Name},
-				{Value: club.Address},
-				{Value: club.Town},
-				{Value: club.Postcode},
-				{Value: fmt.Sprintf("%f", club.Latitude)},
-				{Value: fmt.Sprintf("%f", club.Longitude)},
-				{Value: toB36(club.ID)},
-			}}
-		}
-		if invalidForm.action == clubMoundNew {
-			newMoundForm = invalidForm
-		} else {
-			newMoundForm = form{Fields: []field{
-				{},
-				{},
-				{Value: toB36(club.ID)},
-			}}
-		}*/
 
 	templater(w, page{
 		Title:   "Club Settings",
