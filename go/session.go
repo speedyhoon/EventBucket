b7338abed7f33b771e436452f0d538a2f3bbc271
Cam <eventbucket@hotmail.com>
2016-02-29 20:24:30 +1100

    Add new function to get session form and other forms in the list. Rewrite entries page to use much less code. change form validation settings

--- go/session.go
+++ go/session.go
@@ -114,6 +114,51 @@ func getFormSession(w http.ResponseWriter, r *http.Request, formActions ...uint8
 	return form{Fields: getForm(formActions[0])}
 }
 
+func sessionForms(w http.ResponseWriter, r *http.Request, formActions ...uint8) (*uint8, [][]field) {
+	//Get users session id from request
+	cookies, err := r.Cookie(sessionToken)
+	if err != nil || cookies.Value == "" {
+		//No session found. Return default forms.
+		//		return form{Fields: getForm(formActions[0])}
+		return nil, getForms(formActions...)
+	}
+
+	//Start a read lock to prevent concurrent reads while other parts are executing a write.
+	globalSessions.RLock()
+	contents, ok := globalSessions.m[cookies.Value]
+	globalSessions.RUnlock()
+	if ok {
+		//Clear the session contents as it has been returned to the user.
+		globalSessions.Lock()
+		delete(globalSessions.m, cookies.Value)
+		globalSessions.Unlock()
+
+		//Remove cookie.
+		w.Header().Set("Set-Cookie", fmt.Sprintf("%v=; expires=%v HttpOnly", sessionToken, time.Now().UTC().Add(-sessionExpiryTime).Format(formatGMT)))
+		var forms [][]field
+		for _, action := range formActions {
+			if contents.action == action {
+				//				forms = append(forms, contents)
+				forms = append(forms, contents.Fields)
+			} else {
+				//				forms = append(forms, form{action: action, Fields: getForm(action)})
+				forms = append(forms, getForm(action))
+			}
+		}
+		return &contents.action, forms
+	}
+	return nil, getForms(formActions...)
+}
+
+func getForms(formActions ...uint8) [][]field {
+	var forms [][]field //A group of forms, where each form has several fields
+	for _, action := range formActions {
+		//		forms = append(forms, form{action: action, Fields: getForm(action)})
+		forms = append(forms, getForm(action))
+	}
+	return forms
+}
+
 /*
 
 
