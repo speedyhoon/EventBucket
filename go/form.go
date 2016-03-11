22e3e6d5d8901b4fd5cff3ad2f616b64c45a1b0e
Cam <eventbucket@hotmail.com>
2016-03-12 09:02:27 +1100

    gofmt form.go

--- go/form.go
+++ go/form.go
@@ -100,7 +100,7 @@ func getForm(id uint8) []field {
 		return []field{{
 			name: "e", Required: true, min: .01, max: 65535, step: .01, v8: isValidFloat32,
 		}, {
-			name: "z", Required: true, v8: isValidStr, Options: []option{option{Label:"Metres", Value:"m", Selected:false},{Label:"Kilometres", Value:"k", Selected:false},{Label:"Yards", Value:"y", Selected:false},{Label:"Feet", Value:"f", Selected:false}},
+			name: "z", Required: true, v8: isValidStr, Options: []option{{Label: "Metres", Value: "m", Selected: false}, {Label: "Kilometres", Value: "k", Selected: false}, {Label: "Yards", Value: "y", Selected: false}, {Label: "Feet", Value: "f", Selected: false}},
 		}, {
 			name: "C", v8: isValidID, regex: regexID,
 		}}
@@ -152,9 +152,9 @@ func getForm(id uint8) []field {
 		}, {
 			name: "S", v8: isValidStr,
 		}, {
-			name: "g", Required: true, min: 1, max: 11, step: 1, v8: isValidUint64, Options: []option{option{Label:"", Value:"", Selected:false},{Label:"Target A", Value:"1", Selected:false},{Label:"Target B", Value:"2", Selected:false},{Label:"Target C", Value:"3", Selected:false},{Label:"F Class A", Value:"4", Selected:false},{Label:"F Class B", Value:"5", Selected:false},{Label:"F Class Open", Value:"6", Selected:false},{Label:"F/TR", Value:"7", Selected:false},{Label:"Match Open", Value:"8", Selected:false},{Label:"Match Reserve", Value:"9", Selected:false},{Label:"303 Rifle", Value:"10", Selected:false}},
+			name: "g", Required: true, min: 1, max: 11, step: 1, v8: isValidUint64, Options: []option{{Label: "", Value: "", Selected: false}, {Label: "Target A", Value: "1", Selected: false}, {Label: "Target B", Value: "2", Selected: false}, {Label: "Target C", Value: "3", Selected: false}, {Label: "F Class A", Value: "4", Selected: false}, {Label: "F Class B", Value: "5", Selected: false}, {Label: "F Class Open", Value: "6", Selected: false}, {Label: "F/TR", Value: "7", Selected: false}, {Label: "Match Open", Value: "8", Selected: false}, {Label: "Match Reserve", Value: "9", Selected: false}, {Label: "303 Rifle", Value: "10", Selected: false}},
 		}, {
-			name: "r", min: 1, max: 5, step: 1, v8: isValidUint64, Options: []option{option{Label:"", Value:"", Selected:false},{Label:"Junior U21", Value:"1", Selected:false},{Label:"Junior U25", Value:"2", Selected:false},{Label:"Veteran", Value:"3", Selected:false},{Label:"Super Veteran", Value:"4", Selected:false}},
+			name: "r", min: 1, max: 5, step: 1, v8: isValidUint64, Options: []option{{Label: "", Value: "", Selected: false}, {Label: "Junior U21", Value: "1", Selected: false}, {Label: "Junior U25", Value: "2", Selected: false}, {Label: "Veteran", Value: "3", Selected: false}, {Label: "Super Veteran", Value: "4", Selected: false}},
 		}, {
 			name: "E", v8: isValidID, regex: regexID,
 		}, {
@@ -164,9 +164,9 @@ func getForm(id uint8) []field {
 		return []field{{
 			name: "S", Required: true, v8: isValidID, regex: regexID,
 		}, {
-			name: "g", Required: true, min: 1, max: 11, step: 1, v8: isValidUint64, Options: []option{option{Label:"", Value:"", Selected:false},{Label:"Target A", Value:"1", Selected:false},{Label:"Target B", Value:"2", Selected:false},{Label:"Target C", Value:"3", Selected:false},{Label:"F Class A", Value:"4", Selected:false},{Label:"F Class B", Value:"5", Selected:false},{Label:"F Class Open", Value:"6", Selected:false},{Label:"F/TR", Value:"7", Selected:false},{Label:"Match Open", Value:"8", Selected:false},{Label:"Match Reserve", Value:"9", Selected:false},{Label:"303 Rifle", Value:"10", Selected:false}},
+			name: "g", Required: true, min: 1, max: 11, step: 1, v8: isValidUint64, Options: []option{{Label: "", Value: "", Selected: false}, {Label: "Target A", Value: "1", Selected: false}, {Label: "Target B", Value: "2", Selected: false}, {Label: "Target C", Value: "3", Selected: false}, {Label: "F Class A", Value: "4", Selected: false}, {Label: "F Class B", Value: "5", Selected: false}, {Label: "F Class Open", Value: "6", Selected: false}, {Label: "F/TR", Value: "7", Selected: false}, {Label: "Match Open", Value: "8", Selected: false}, {Label: "Match Reserve", Value: "9", Selected: false}, {Label: "303 Rifle", Value: "10", Selected: false}},
 		}, {
-			name: "r", min: 1, max: 5, step: 1, v8: isValidUint64, Options: []option{option{Label:"", Value:"", Selected:false},{Label:"Junior U21", Value:"1", Selected:false},{Label:"Junior U25", Value:"2", Selected:false},{Label:"Veteran", Value:"3", Selected:false},{Label:"Super Veteran", Value:"4", Selected:false}},
+			name: "r", min: 1, max: 5, step: 1, v8: isValidUint64, Options: []option{{Label: "", Value: "", Selected: false}, {Label: "Junior U21", Value: "1", Selected: false}, {Label: "Junior U25", Value: "2", Selected: false}, {Label: "Veteran", Value: "3", Selected: false}, {Label: "Super Veteran", Value: "4", Selected: false}},
 		}, {
 			name: "E", Required: true, v8: isValidID, regex: regexID,
 		}}
@@ -186,9 +186,9 @@ func getForm(id uint8) []field {
 		}, {
 			name: "C", Required: true, maxLen: 64, v8: isValidStr,
 		}, {
-			name: "g", Required: true, min: 1, max: 11, step: 1, v8: isValidUint64, Options: []option{option{Label:"", Value:"", Selected:false},{Label:"Target A", Value:"1", Selected:false},{Label:"Target B", Value:"2", Selected:false},{Label:"Target C", Value:"3", Selected:false},{Label:"F Class A", Value:"4", Selected:false},{Label:"F Class B", Value:"5", Selected:false},{Label:"F Class Open", Value:"6", Selected:false},{Label:"F/TR", Value:"7", Selected:false},{Label:"Match Open", Value:"8", Selected:false},{Label:"Match Reserve", Value:"9", Selected:false},{Label:"303 Rifle", Value:"10", Selected:false}},
+			name: "g", Required: true, min: 1, max: 11, step: 1, v8: isValidUint64, Options: []option{{Label: "", Value: "", Selected: false}, {Label: "Target A", Value: "1", Selected: false}, {Label: "Target B", Value: "2", Selected: false}, {Label: "Target C", Value: "3", Selected: false}, {Label: "F Class A", Value: "4", Selected: false}, {Label: "F Class B", Value: "5", Selected: false}, {Label: "F Class Open", Value: "6", Selected: false}, {Label: "F/TR", Value: "7", Selected: false}, {Label: "Match Open", Value: "8", Selected: false}, {Label: "Match Reserve", Value: "9", Selected: false}, {Label: "303 Rifle", Value: "10", Selected: false}},
 		}, {
-			name: "r", min: 1, max: 5, step: 1, v8: isValidUint64, Options: []option{option{Label:"", Value:"", Selected:false},{Label:"Junior U21", Value:"1", Selected:false},{Label:"Junior U25", Value:"2", Selected:false},{Label:"Veteran", Value:"3", Selected:false},{Label:"Super Veteran", Value:"4", Selected:false}},
+			name: "r", min: 1, max: 5, step: 1, v8: isValidUint64, Options: []option{{Label: "", Value: "", Selected: false}, {Label: "Junior U21", Value: "1", Selected: false}, {Label: "Junior U25", Value: "2", Selected: false}, {Label: "Veteran", Value: "3", Selected: false}, {Label: "Super Veteran", Value: "4", Selected: false}},
 		}}
 	case 11:
 		return []field{{
@@ -200,9 +200,9 @@ func getForm(id uint8) []field {
 		}, {
 			name: "C", Required: true, maxLen: 64, v8: isValidStr,
 		}, {
-			name: "g", Required: true, min: 1, max: 11, step: 1, v8: isValidUint64, Options: []option{option{Label:"", Value:"", Selected:false},{Label:"Target A", Value:"1", Selected:false},{Label:"Target B", Value:"2", Selected:false},{Label:"Target C", Value:"3", Selected:false},{Label:"F Class A", Value:"4", Selected:false},{Label:"F Class B", Value:"5", Selected:false},{Label:"F Class Open", Value:"6", Selected:false},{Label:"F/TR", Value:"7", Selected:false},{Label:"Match Open", Value:"8", Selected:false},{Label:"Match Reserve", Value:"9", Selected:false},{Label:"303 Rifle", Value:"10", Selected:false}},
+			name: "g", Required: true, min: 1, max: 11, step: 1, v8: isValidUint64, Options: []option{{Label: "", Value: "", Selected: false}, {Label: "Target A", Value: "1", Selected: false}, {Label: "Target B", Value: "2", Selected: false}, {Label: "Target C", Value: "3", Selected: false}, {Label: "F Class A", Value: "4", Selected: false}, {Label: "F Class B", Value: "5", Selected: false}, {Label: "F Class Open", Value: "6", Selected: false}, {Label: "F/TR", Value: "7", Selected: false}, {Label: "Match Open", Value: "8", Selected: false}, {Label: "Match Reserve", Value: "9", Selected: false}, {Label: "303 Rifle", Value: "10", Selected: false}},
 		}, {
-			name: "r", min: 1, max: 5, step: 1, v8: isValidUint64, Options: []option{option{Label:"", Value:"", Selected:false},{Label:"Junior U21", Value:"1", Selected:false},{Label:"Junior U25", Value:"2", Selected:false},{Label:"Veteran", Value:"3", Selected:false},{Label:"Super Veteran", Value:"4", Selected:false}},
+			name: "r", min: 1, max: 5, step: 1, v8: isValidUint64, Options: []option{{Label: "", Value: "", Selected: false}, {Label: "Junior U21", Value: "1", Selected: false}, {Label: "Junior U25", Value: "2", Selected: false}, {Label: "Veteran", Value: "3", Selected: false}, {Label: "Super Veteran", Value: "4", Selected: false}},
 		}}
 	case 12:
 		return []field{{
