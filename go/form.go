c88a9a0b9da2cfa92345b6863625d0e3771f0eef
Cam <eventbucket@hotmail.com>
2016-03-12 17:03:58 +1100

    remove default values for lat & longitude

--- go/form.go
+++ go/form.go
@@ -86,9 +86,9 @@ func getForm(id uint8) []field {
 		}, {
 			name: "p", maxLen: 64, v8: isValidStr,
 		}, {
-			name: "x", Value: "-29", min: -90, max: 90, step: .000001, v8: isValidFloat32,
+			name: "x", min: -90, max: 90, step: .000001, v8: isValidFloat32,
 		}, {
-			name: "y", Value: "141", min: -180, max: 180, step: .000001, v8: isValidFloat32,
+			name: "y", min: -180, max: 180, step: .000001, v8: isValidFloat32,
 		}, {
 			name: "b", v8: isValidBool,
 		}, {
