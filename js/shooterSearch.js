517f0e7d750989b83a978cc5fb77131c446b991e
Cam <eventbucket@hotmail.com>
2016-03-13 21:30:07 +1100

    improve shooter entry form

--- js/shooterSearch.js
+++ js/shooterSearch.js
@@ -9,10 +9,16 @@ function getter(form) {
   var xhttp = new XMLHttpRequest;
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4) {
+      if (xhttp.responseText.length) {
+        form.S.parentElement.removeAttribute('hidden');
+      }else {
+        form.S.parentElement.setAttribute('hidden','');
+      }
+      form.S.removeAttribute('class');
       if (xhttp.status == 200) {
         form.S.innerHTML = xhttp.responseText;
-        form.S.removeAttribute('class');
       }else {
+        //TODO possibly don't need this below line now
         form.S.setAttribute('class', 'not200');
       }
     }
