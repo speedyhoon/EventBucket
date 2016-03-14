f55e1dd7114e792042b202d66ecf25fc90028027
sh <16303791+speedyhoon@users.noreply.github.com>
2016-03-14 20:13:25 +1100

    Total Scores page working with save form going to database & displaying results properly

--- go/pTotalScores.go
+++ go/pTotalScores.go
@@ -17,7 +17,29 @@ func totalScores(w http.ResponseWriter, r *http.Request, eventID string) {
 		MenuID:  eventID,
 		Heading: event.Name,
 		Data: map[string]interface{}{
-			"Event": event,
+			"EventID":  event.ID,
+			"Shooters": event.Shooters,
+			"Range":    event.Ranges[7],
 		},
 	})
+}
+
+func eventTotalUpsert(w http.ResponseWriter, r *http.Request, submittedForm form, redirect func()) {
+	eventID := submittedForm.Fields[2].Value
+	rangeID := submittedForm.Fields[3].Value
+	shooterID := submittedForm.Fields[4].internalValue.(uint64)
+
+	//Insert new event into database.
+	err := upsertScore(eventID, rangeID, shooterID, Score{
+		Total:   submittedForm.Fields[0].internalValue.(uint64),
+		Centers: submittedForm.Fields[1].internalValue.(uint64),
+	})
+
+	//Display any upsert errors onscreen.
+	if err != nil {
+		formError(w, submittedForm, redirect, err)
+		return
+	}
+	http.Redirect(w, r, urlTotalScores+eventID /*+"/"_rangeID*/, http.StatusSeeOther)
+	//TODO trigger agg calculation immediatly. or maybe inline it within the same DB call?
 }
