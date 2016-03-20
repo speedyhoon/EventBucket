f0e1770bf52439189b16269e66a9a11c1b420bae
Cam <eventbucket@hotmail.com>
2016-03-20 11:04:34 +1100

    Initial scoreboard page without sorting yet.

--- go/pScoreboard.go
+++ go/pScoreboard.go
@@ -17,7 +17,28 @@ func scoreboard(w http.ResponseWriter, r *http.Request, eventID string) {
 		MenuID:  eventID,
 		Heading: event.Name,
 		Data: map[string]interface{}{
-			"Event": event,
+			"Event":       event,
+			"Legend":      scoreBoardLegend(),
+			"SortByRange": 3,
 		},
 	})
+}
+
+type legend struct {
+	//To access a field in HTML a struct, it must start with an uppercase letter. Other wise it will output error: xxx is an unexported field of struct type main.legend
+	Class, Name string
+}
+
+func scoreBoardLegend() []legend {
+	//Constants are not able to be slices so using a function instead.
+	//Using a Legend slice because a map[string]string would render with a random order.
+	return []legend{
+		{Class: "^highestPossibleScore^", Name: "Highest Possible Score"},
+		{Class: "^shootOff^", Name: "Shoot Off"},
+		{Class: "^incompleteScore^", Name: "Incomplete Score"},
+		{Class: "^noScore^", Name: "Missing Score"},
+		{Class: "^p1^", Name: "1st"},
+		{Class: "^p2^", Name: "2nd"},
+		{Class: "^p3^", Name: "3rd"},
+	}
 }
