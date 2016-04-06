814eaca5cc5decf9af80804e8a2431788d2db955
sh <16303791+speedyhoon@users.noreply.github.com>
2016-04-06 19:57:42 +1000

    Create edit.svg which is 244 bytes compared to edit.png that is 6.5KB remove edit.png insert edit icon into sass replace edit icon images in table with css background condense shooter search javascript file.

--- js/shooterSearch.js
+++ js/shooterSearch.js
@@ -1,29 +1,23 @@
 'use strict';
-var f = '', s = '', c = '';
+var xhr = new XMLHttpRequest, f = '', s = '', c = '';
 function getter(form) {
   //Ignore form inputs into other fields
   if (f === form.f.value && s === form.s.value && c === form.C.value) return;
   f = form.f.value;
   s = form.s.value;
   c = form.C.value;
-  var xhttp = new XMLHttpRequest;
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4) {
-      if (xhttp.responseText.length) {
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      form.S.removeAttribute('class');
+      if (xhr.status == 200 && xhr.responseText.length) {
+        form.S.innerHTML = xhr.responseText;
         form.S.parentElement.removeAttribute('hidden');
       }else {
         form.S.parentElement.setAttribute('hidden','');
       }
-      form.S.removeAttribute('class');
-      if (xhttp.status == 200) {
-        form.S.innerHTML = xhttp.responseText;
-      }else {
-        //TODO possibly don't need this below line now
-        form.S.setAttribute('class', 'not200');
-      }
     }
   };
   form.S.setAttribute('class', 'loading');
-  xhttp.open('GET', '/9?f=' + f + '&s=' + s + '&C=' + c, true);
-  xhttp.send();
+  xhr.open('GET', '/9' + (f ? '?f=' + f : '') + (s ? '?s=' + s : '') + (c ? '?c=' + c : ''), true);
+  xhr.send();
 }
