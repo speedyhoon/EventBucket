704d4ea72c44abc5c14dd28cc4c7a5a3df446dcc
sh <16303791+speedyhoon@users.noreply.github.com>
2016-04-07 07:53:41 +1000

    move base36 string to uint and string to uint function into architecture dependant files. So golang can compile to different version of code it needs.

---
+++ go/utils_amd64.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+//Converts base36 string to uint
+func b36tou(id string) (uint, error) {
+	trace.Println("64 bit version")
+	u, err := strconv.ParseUint(strings.TrimSpace(id), 36, 64)
+	return uint(u), err
+}
+
+//Converts base36 string to uint
+func strToUint(id string) (uint, error) {
+	trace.Println("64 bit version")
+	u, err := strconv.ParseUint(strings.TrimSpace(id), 10, 64)
+	return uint(u), err
+}
