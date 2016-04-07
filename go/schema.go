df687bab9997ca7d8bcccb93dfb5fabfef4a01ac
sh <16303791+speedyhoon@users.noreply.github.com>
2016-04-08 07:10:45 +1000

    Remove schema constants from schema.go

--- go/schema.go
+++ go/schema.go
@@ -7,45 +7,46 @@ import (
 
 // Club is exported
 type Club struct {
-	ID        string  `json:"schemaID"`
-	Name      string  `json:"schemaName"`
-	IsDefault bool    `json:"schemaIsDefault,omitempty"`
-	Mounds    []Mound `json:"schemaMound,omitempty"`
-	Latitude  float32 `json:"schemaLatitude,omitempty"`
-	Longitude float32 `json:"schemaLongitude,omitempty"`
-	URL       string  `json:"schemaURL,omitempty"`
-	Address   string  `json:"schemaAddress,omitempty"`
-	Town      string  `json:"schemaTown,omitempty"`
-	Postcode  string  `json:"schemaPostcode,omitempty"`
-	AutoInc   AutoInc `json:"schemaAutoInc,omitempty"`
+	ID        string  `json:"I"`
+	Name      string  `json:"n"`
+	IsDefault bool    `json:"d,omitempty"`
+	Mounds    []Mound `json:"M,omitempty"`
+	Latitude  float32 `json:"x,omitempty"`
+	Longitude float32 `json:"y,omitempty"`
+	URL       string  `json:"u,omitempty"`
+	Address   string  `json:"a,omitempty"`
+	Town      string  `json:"t,omitempty"`
+	Postcode  string  `json:"p,omitempty"`
+	AutoInc   AutoInc `json:"A,omitempty"`
 }
 
 // AutoInc is a auto increment counter
 type AutoInc struct {
-	Mound   uint `json:"schemaMound,omitempty"`
-	Event   uint `json:"schemaEvent,omitempty"`
-	Club    uint `json:"schemaClub,omitempty"`
-	Range   uint `json:"schemaRange,omitempty"`
-	Shooter uint `json:"schemaShooter,omitempty"`
+	Mound   uint `json:"M,omitempty"`
+	Event   uint `json:"E,omitempty"`
+	Club    uint `json:"C,omitempty"`
+	Range   uint `json:"R,omitempty"`
+	Shooter uint `json:"S,omitempty"`
 }
 
 // Mound is exported
 type Mound struct {
-	//	ID       string `json:"schemaID"`
-	Distance uint   `json:"schemaDistance,omitempty"`
-	Unit     string `json:"schemaUnit,omitempty"`
+	//	ID       string `json:"I"`
+	Distance uint   `json:"d,omitempty"`
+	Unit     string `json:"u,omitempty"`
 }
 
 // Event is exported
 type Event struct {
-	ID       string         `json:"schemaID"`
-	Name     string         `json:"schemaName"`
-	Club     string         `json:"schemaClub"`
-	Date     string         `json:"schemaDate"`
-	Time     string         `json:"schemaTime"`
-	Ranges   []Range        `json:"schemaRange,omitempty"`
-	AutoInc  AutoInc        `json:"schemaAutoInc"`
-	Shooters []EventShooter `json:"schemaShooter,omitempty"`
+	ID       string         `json:"I"`
+	Name     string         `json:"n"`
+	ClubID   string         `json:"C"`
+	Club     string         `json:"c"`
+	Date     string         `json:"d"`
+	Time     string         `json:"t"`
+	Ranges   []Range        `json:"R,omitempty"`
+	AutoInc  AutoInc        `json:"A"`
+	Shooters []EventShooter `json:"S,omitempty"`
 	Closed   bool           `json:"schemaClosed,omitempty"`
 	/*Grades       []uint         `json:"schemaGrades,omitempty"`
 	SortScoreboard string         `json:"o,omitempty"`
@@ -57,13 +58,13 @@ type Event struct {
 
 // Range is exported
 type Range struct {
-	ID     uint   `json:"schemaID"`
-	Name   string `json:"schemaName"`
-	Aggs   []uint `json:"schemaAggregate,omitempty"`
-	Locked bool   `json:"schemaLocked,omitempty"`
-	IsAgg  bool   `json:"schemaIsAgg,omitempty"` //Prevents aggs switching to normal ranges
-	Order  int    `json:"schemaSort,omitempty"`
-	Status uint8  `json:"schemaStatus,omitempty"` //ENUM change to 1 when the first shooter has recorded their first shot change to 2 when the range is finished. http://stackoverflow.com/questions/14426366/what-is-an-idiomatic-way-of-representing-enums-in-golang
+	ID     uint   `json:"I"`
+	Name   string `json:"n"`
+	Aggs   []uint `json:"a,omitempty"`
+	Locked bool   `json:"l,omitempty"`
+	IsAgg  bool   `json:"i,omitempty"` //Prevents aggs switching to normal ranges
+	Order  int    `json:"o,omitempty"`
+	Status uint8  `json:"u,omitempty"` //ENUM change to 1 when the first shooter has recorded their first shot change to 2 when the range is finished. http://stackoverflow.com/questions/14426366/what-is-an-idiomatic-way-of-representing-enums-in-golang
 	//Class      map[string]RangeProperty `json:"omitempty,inline"` //TODO possibly change it to optional grades per range in future
 	//ScoreBoard bool                     `json:"s,omitempty"`
 	//Hidden     bool                     `json:"h,omitempty"`
@@ -77,12 +78,12 @@ func (r Range) strID() string {
 // Score is exported
 type Score struct {
 	//TODO the schema should change so that it can use unsigned  bit numbers instead
-	Total      uint   `json:"schemaTotal"`
-	Centers    uint   `json:"schemaCenters,omitempty"`
+	Total      uint   `json:"t"`
+	Centers    uint   `json:"c,omitempty"`
 	Shots      string `json:"s,omitempty"` //Don't include this in the scoreboard struct when using a different []EventShooter
 	CountBack  string `json:"v,omitempty"`
 	CountBack2 string `json:"x,omitempty"`
-	ShootOff   uint   `json:"f,omitempty"`
+	ShootOff   uint   `json:"s,omitempty"`
 	//position  int    `json:"p,omitempty"` //DON'T SAVE THIS TO DB! used for scoreboard only.
 	//warning   uint8    `json:"w,omitempty"` //DON'T SAVE THIS TO DB! used for scoreboard only.
 	//Ordinal   string `json:"o,omitempty"`
@@ -91,17 +92,17 @@ type Score struct {
 
 // EventShooter is exported
 type EventShooter struct {
-	ID        uint             `json:"schemaID"`
-	FirstName string           `json:"schemaFirstName"` //TODO change these to point to shooters in the other shooter tables
-	Surname   string           `json:"schemaSurname"`
-	Club      string           `json:"schemaClub"`
-	Grade     uint             `json:"schemaGrade"`
-	Hidden    bool             `json:"schemaHidden,omitempty"`
-	AgeGroup  uint             `json:"schemaAgeGroup,omitempty"`
-	Scores    map[string]Score `json:"schemaScores,omitempty"`   //Scores   []Score `json:"schemaScores,omitempty"`   //S is not used!
-	LinkedID  uint             `json:"schemaLinkedID,omitempty"` //For duplicating shooters that are in different classes with the same score
-	SID       int              `json:"schemaSID,omitempty"`
-	Disabled  bool             `json:"schemaDisabled,omitempty"`
+	ID        uint             `json:"I"`
+	FirstName string           `json:"f"` //TODO change these to point to shooters in the other shooter tables
+	Surname   string           `json:"s"`
+	Club      string           `json:"C"`
+	Grade     uint             `json:"g"`
+	Hidden    bool             `json:"h,omitempty"`
+	AgeGroup  uint             `json:"r,omitempty"`
+	Scores    map[string]Score `json:"S,omitempty"` //Scores   []Score `json:"schemaScores,omitempty"`   //S is not used!
+	LinkedID  uint             `json:"l,omitempty"` //For duplicating shooters that are in different classes with the same score
+	SID       int              `json:"M,omitempty"`
+	Disabled  bool             `json:"d,omitempty"`
 	//SCOREBOARD
 	position string //DON'T SAVE THIS TO DB! used for scoreboard only.
 	warning  uint8  //DON'T SAVE THIS TO DB! used for scoreboard only.
@@ -118,25 +119,25 @@ type EventShooter struct {
 
 // Shooter is exported
 type Shooter struct {
-	ID        string `json:"schemaID"`
-	SID       int    `json:"schemaSID,omitempty"`
-	NraaID    int    `json:"schemaNraaID,omitempty"`
-	Surname   string `json:"schemaSurname,omitempty"`
-	FirstName string `json:"schemaFirstName,omitempty"`
-	NickName  string `json:"schemaNickName,omitempty"`
-	Club      string `json:"schemaClub,omitempty"`
+	ID        string `json:"I"`
+	SID       int    `json:"M,omitempty"`
+	NraaID    int    `json:"N,omitempty"`
+	Surname   string `json:"s,omitempty"`
+	FirstName string `json:"f,omitempty"`
+	NickName  string `json:"n,omitempty"`
+	Club      string `json:"C,omitempty"`
 	//Skill map[string]Skill	//Grading set by the VRA for each class
-	Address string `json:"schemaAddress,omitempty"`
-	Email   string `json:"schemaEmail,omitempty"`
+	Address string `json:"a,omitempty"`
+	Email   string `json:"e,omitempty"`
 	//Shooter details 0=not modified, 1=updated, 2=merged, 3=deleted
-	Status int `json:"schemaStatus,omitempty"`
+	Status int `json:"v,omitempty"`
 	//If shooter details are merged with another existing shooter then this is the other NRAA_SID it was merged with
 	//When merging set one record to merged, the other to deleted.
 	//Both records must set MergedSID to the other corresponding shooter SID
-	MergedSID int `json:"schemaMergedSID,omitempty"`
+	MergedSID int `json:"m,omitempty"`
 
-	AgeGroup uint `json:"schemaAgeGroup,omitempty"`
-	Grade    uint `json:"schemaGrade"`
+	AgeGroup uint `json:"r,omitempty"`
+	Grade    uint `json:"g"`
 }
 
 type field struct {
