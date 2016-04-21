3d9e3f22caaa9018fa25dcede896221eca1a33fd
sh <16303791+speedyhoon@users.noreply.github.com>
2016-04-21 23:11:45 +1000

    connecting up start shooting and displaying the saved scores

--- js/startShooting.js
+++ js/startShooting.js
@@ -1,21 +1,21 @@
 (function(){
 	'use strict';
-	var currentCell, currentRow, j, classes = {
+	var currentCell, currentRow, classes = {
 		currentType:null,
 		0:{
 			sighters:2,
 			validShots:'012345V6X',
 			validScore:'012345555',
 			validCenta:'000000111',
-			validSighters:')!@#$%v^x',
+			validSighters:'abcdefvvx',
 			buttons:'012345VX'
 		},
 		1:{
-			sighters:2,
+			sighters:3,
 			validShots:'012345V6X',
 			validScore:'012345666',
 			validCenta:'000000001',
-			validSighters:')!@#$%v^x',
+			validSighters:'abcdefggx',
 			buttons:'0123456X'
 		},
 		2:{
@@ -23,12 +23,12 @@
 			validShots:'012345V6X',
 			validScore:'012345555',
 			validCenta:'000000111',
-			validSighters:')!@#$%v^x',
+			validSighters:'abcdefvvx',
 			buttons:'012345VX'
 		}
 	};
 	if(window.XMLHttpRequest){
-		j = new XMLHttpRequest();
+		var j = new XMLHttpRequest();
 	}
 	function getShootersClass(){
 		return currentRow.getAttribute('data-class');
@@ -47,23 +47,23 @@
 	}
 
 	function recalculateTotal(value){
-		var type = getShootersClass(), index = classes[type].validShots.indexOf(value), newer = ~~classes[type].validScore[index], newerC = ~~classes[type].validCenta[index], total, centres;
-		currentCell.innerHTML = value;
+		var type = getShootersClass(), index = classes[type].validShots.indexOf(value), newer = ~~classes[type].validScore[index], newerC = ~~classes[type].validCenta[index], total, centers;
+		currentCell.textContent = value;
 		if(getCurrentNth() >= getNoOfSighters()){
 			total = getValue(currentRow, 'total') - getValue(currentCell, 'value') + newer;
-			centres = getValue(currentRow, 'centres') - getValue(currentCell, 'center') + newerC;
-			currentRow.lastChild.innerHTML = total + '<sup>' + centres + '</sup>';
+			centers = getValue(currentRow, 'centers') - getValue(currentCell, 'center') + newerC;
+			currentRow.lastChild.innerHTML = total + (centers ? '<sup>' + centers + '</sup>' : '');
 			currentRow.setAttribute('data-total', total);
-			currentRow.setAttribute('data-centres', centres);
+			currentRow.setAttribute('data-centers', centers);
 		}
 		currentCell.setAttribute('data-value', newer);
 		currentCell.setAttribute('data-center', newerC);
 	}
 
-	function getAjax(){
-		var cells = currentRow.getElementsByTagName('td'), shootersClass = classes[getShootersClass()], send = '', sighters = getNoOfSighters(), value = '', i, max = cells.length, index;
-		for(i = 0; i < max; i++){
-			value = cells[i].innerHTML;
+	function getShots(){
+		var cells = currentRow.getElementsByTagName('td'), shootersClass = classes[getShootersClass()], send = '', sighters = getNoOfSighters(), value = '', i=-1, max = cells.length, index;
+		while(++i<max){
+			value = cells[i].textContent;
 			if(!value){
 				send += '-';
 			}else if(i < sighters){
@@ -79,17 +79,18 @@
 				send += value;
 			}
 		}
-		return send;
+		return send.replace(/-+$/, '');  //trim training hyphens
 	}
 
 	function ajax(id){
-		var shots = getAjax(), table = document.querySelector('table'), eventID = table.getAttribute('data-eventID'), rangeID = table.getAttribute('data-rangeID');
-		shots = encodeURI(shots).replace(/#/gi, '%23');	//hashes are converted after encodeURI to stop % being converted twice
-		j.open('POST', '/16?E=' + eventID + '&R=' + rangeID + '&S=' + id + '&s=' + shots, true);
-		j.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+		var table = document.querySelector('table'), eventID = table.getAttribute('data-eventID'), rangeID = table.getAttribute('data-rangeID');
+		//shots = encodeURI(getShots()).replace(/#/gi, '%23');	//hashes are converted after encodeURI to stop % being converted twice
+		j.open('POST', '/16?E=' + eventID + '&R=' + rangeID + '&S=' + id + '&s=' + getShots(), true);
+		//j.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 		j.send();
-		j.onreadystatechange = function(){
+		j.onreadystatechange = function stateChanger(){
 			if(j.readyState == 4){
+				//TODO status ok && html == same - GREEN    else    RED
 				currentRow.querySelector('.t').innerHTML = j.status === 200 ? j.response : 'failed';
 			}
 		};
@@ -109,7 +110,7 @@
 
 	function changeValue(value){
 		if(currentCell){
-			if(currentCell.innerHTML !== value){//prevents recalculating the score if it is the same value
+			if(currentCell.textContent !== value){//prevents recalculating the score if it is the same value
 				recalculateTotal(value);
 				ajax(currentRow.getAttribute('id'));
 			}
@@ -119,73 +120,67 @@
 		}
 	}
 
-	function buttonsAddEvents(){
-		var buttons = document.getElementsByTagName('button'), max = buttons.length, buttonOnClickEvent = function(buttonValue){
-			return function(){
-				changeValue(buttonValue);
-			};
-		};
-		while(max--){
-			buttons[max].onclick = buttonOnClickEvent(buttons[max].innerHTML);
-		}
-	}
-
 	function generateButtons(){
-		var type = getShootersClass(), buttonLength, buttonHtml, n;
-		if(classes.currentType !== type){
+		var type = getShootersClass();
+		if(type && classes.currentType !== type && classes[type].buttons){
 			classes.currentType = type;
-			if(type && classes[type].buttons){
-				buttonLength = classes[type].buttons.length;
-				buttonHtml = '';
-				for(n = 0; n < buttonLength; n++){
-					buttonHtml += '<button>' + classes[type].buttons[n] + '</button>';
-				}
-				document.getElementById('bu').innerHTML = buttonHtml;
-				buttonsAddEvents();
+			var h=-1, td = document.createElement('td'), buttonLength = classes[type].buttons.length, buttonOnClickEvent = function buttonClickEventer(buttonValue){
+				return function buttonClicker(){
+					changeValue(buttonValue);
+				};
+			};
+			while(++h < buttonLength){
+				var button = document.createElement('button');
+				button.textContent = classes[type].buttons[h];
+				button.onclick = buttonOnClickEvent(classes[type].buttons[h]);
+				td.appendChild(button);
 			}
+			var buttonsCell = document.getElementById('bu');
+			td.id='bu';
+			td.setAttribute('colspan', buttonsCell.getAttribute('colspan'));
+			buttonsCell.parentNode.replaceChild(td, buttonsCell);
 		}
 	}
 
 	function moveHeader(){
-		var tbody = currentRow.parentNode;
-		tbody.insertBefore(document.getElementById('h'), currentRow);
-		tbody.insertBefore(document.getElementById('x'), currentRow.nextSibling);//equivilent to insertAfter!
+		currentRow.parentNode.insertBefore(document.getElementById('h'), currentRow);
+		currentRow.parentNode.insertBefore(document.getElementById('x'), currentRow.nextSibling);//equivilent to insertAfter!
 		generateButtons();
 	}
 
 	/*	function changeSighters(){//when changing the value of the select box "sighters"
-						var selected = document.getElementById('selectSighters').selectedIndex, tds, iteration = 0, sighters, selectedCell, i;
-						if(currentRow && selected > 0){
-							tds = currentRow.getElementsByTagName('td');
-							sighters = getNoOfSighters();
-							selectedCell = currentCell;
-							for(i = sighters - selected; i < sighters; i++){
-								currentCell = tds[sighters + iteration++];//increment iteration AFTER this line
-								recalculateTotal(tds[i].innerHTML);
-							}
-							currentCell = selectedCell;
-							highlightOnlyTheCell(tds[(sighters + selected)]);
-							ajax(currentRow.getAttribute('id'));
-						}
-					}*/
-	/*	function modifySelectBox(){//alter the options in the sighters select box for different classes
-						var additional = '', k, selectBox = document.createElement('select'), selectSighters;
-						for(k = getNoOfSighters(); k >= 2; k--){
-							additional += '<option>Keep S' + k + ' &gt;</option>';
-						}
-						selectBox.id = 'selectSighters';
-						selectBox.innerHTML = '<option>Drop All</option>' + additional + '<option>Keep All</option>';
-						if(currentRow.getAttribute('data-sighters')){
-							selectBox.selectedIndex = currentRow.getAttribute('data-sighters');
-						}
-						selectBox.onchange = function(){
-							return function(){
-								changeSighters();
-							};
-						};
-						selectSighters = document.getElementById('selectSighters');
-						selectSighters.parentNode.replaceChild(selectBox, selectSighters);
-					}*/
+		var selected = document.getElementById('selectSighters').selectedIndex, tds, iteration = 0, sighters, selectedCell, i;
+		if(currentRow && selected > 0){
+			tds = currentRow.getElementsByTagName('td');
+			sighters = getNoOfSighters();
+			selectedCell = currentCell;
+			for(i = sighters - selected; i < sighters; i++){
+				currentCell = tds[sighters + iteration++];//increment iteration AFTER this line
+				recalculateTotal(tds[i].textContent);
+			}
+			currentCell = selectedCell;
+			highlightOnlyTheCell(tds[(sighters + selected)]);
+			ajax(currentRow.getAttribute('id'));
+		}
+	}
+	function modifySelectBox(){//alter the options in the sighters select box for different classes
+		var additional = '', k, selectBox = document.createElement('select'), selectSighters;
+		for(k = getNoOfSighters(); k >= 2; k--){
+			additional += '<option>Keep S' + k + ' &gt;</option>';
+		}
+		selectBox.id = 'selectSighters';
+		selectBox.innerHTML = '<option>Drop All</option>' + additional + '<option>Keep All</option>';
+		if(currentRow.getAttribute('data-sighters')){
+			selectBox.selectedIndex = currentRow.getAttribute('data-sighters');
+		}
+		selectBox.onchange = function(){
+			return function(){
+				changeSighters();
+			};
+		};
+		selectSighters = document.getElementById('selectSighters');
+		selectSighters.parentNode.replaceChild(selectBox, selectSighters);
+	}*/
 	function highlightRow(row){//HIGHLIGHT THE SELECTED ROW
 		if(row !== currentRow){
 			row.setAttribute('data-selected', '1');
@@ -193,7 +188,7 @@
 				currentRow.removeAttribute('data-selected');
 			}
 			currentRow = row;
-			highlightOnlyTheCell(row.getElementsByTagName('td')[0]);
+			highlightOnlyTheCell(row.querySelector('td'));
 			moveHeader();
 			//modifySelectBox();
 		}
@@ -208,24 +203,19 @@
 		}
 	}
 
-	function listenToTds(){
-		var tdElements = currentRow.getElementsByTagName('td'), tdQuantity = tdElements.length, tdOnClickEvent = function(tdElement){
-			return function(){
-				highlightCell(tdElement);
-			};
-		};
-		while(tdQuantity--){
-			tdElements[tdQuantity].onclick = tdOnClickEvent(tdElements[tdQuantity]);
-		}
-		currentRow.setAttribute('data-visited', 1);
-	}
-
 	function runOnLoad(){
 		var shooters = document.querySelectorAll('tbody th:nth-child(4)'), shooterQty = shooters.length, shooterNameOnclick = function(trElement){
-			return function(){
+			return function shooterClick(){
 				highlightRow(trElement);
 				if(!currentRow.getAttribute('data-visited')){	//if the attribute is present it has already been processed
-					listenToTds();
+					currentRow.onclick = function tdClicker(tdElement){
+						return function tdClick(event){
+							if(event.target.nodeName === 'TD'){
+								highlightCell(tdElement);
+							}
+						};
+					};
+					currentRow.setAttribute('data-visited', 1);
 				}
 			};
 		};
