567544ee5d188d93b15579ece731dadc4611a21c
sh <16303791+speedyhoon@users.noreply.github.com>
2016-04-26 19:46:11 +1000

    Fix getSearchShooters not returning the correct Shooters details.  Shooter details that weren't set would be populated by the previous shooter.  Fixed by moving the shooter variable into the for loop so it is destroyed on every loop iteration. Return to previous search page on return change shooter details textbox to a datalist search box  Fix shooter details when editing several shooters only one shooter is affected at a time  Fix grades list select box not populating values properly

--- js/shooterDetails.js
+++ js/shooterDetails.js
@@ -1,7 +1,8 @@
 'use strict';
 function stuff(row) {
   row = row.parentElement;
-  var tds = row.children, t = document.importNode(document.querySelector('template').content, true);
+  var tds = row.children, t = document.importNode(document.querySelector('template').content, true), shooterID = '_' +tds[0].textContent;
+	t.querySelector('form').id = shooterID;
   t.querySelector('[name=s]').value = row.children[1].querySelector('span').textContent;
   tds[1].querySelector('span').textContent = '';
   t.querySelector('[name=f]').value = tds[1].textContent.trim();
@@ -12,13 +13,19 @@ function stuff(row) {
   t.querySelector('[name=I]').value = tds[0].textContent;
   t.querySelector('td').textContent = tds[0].textContent;
 
+	var outerFormFields = t.querySelectorAll('[form=editShooter]'), index = outerFormFields.length;
+	while(index--){
+		outerFormFields[index].setAttribute('form', shooterID);
+	}
+
   row.innerHTML = '';
   row.appendChild(t);
 }
 function findValue(element, label) {
   var options = element.options, i = options.length;
+	label = label.trim();
   while (i--) {
-    if (label === options[i].textContent) {
+    if (label === options[i].textContent.trim()) {
       return options[i].value;
     }
   }
