d6eedbf2af19b36210d5fb6dd044e00e96663dc5
Cam <eventbucket@hotmail.com>
2016-04-29 17:51:39 +1000

    Add code for comments

--- go/template.go
+++ go/template.go
@@ -190,6 +190,23 @@ func templater(w http.ResponseWriter, page page) {
 }
 
 //AddQuotes returns value with or without surrounding single or double quote characters suitable for a [[//dev.w3.org/html5/html-author/#attributes][HTML5 attribute]] value.
+/*func addQuotes(value string) string {
+	//Contains a single quote and a double quote character.
+	if strings.Contains(value, "'") && strings.Contains(value, `"`) {
+		warn.Printf("HTML attribute value %v contains both single & double quotes", value)
+	}
+	//Space, single quote, accent, equals, less-than sign, greater-than sign.
+	if strings.ContainsAny(value, " '`=<>") {
+		return `"` + value + `"`
+	}
+	//Double quote
+	if strings.Contains(value, `"`) {
+		return "'" + value + "'"
+	}
+	return value
+}*/
+
+//AddQuotes returns value with or without surrounding single or double quote characters suitable for a [[//dev.w3.org/html5/html-author/#attributes][HTML5 attribute]] value.
 func addQuotes(value string) string {
 	//TODO escape any rune character over X code point
 	//	value = html.EscapeString(value)
