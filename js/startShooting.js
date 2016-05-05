01f563b4a8887547248386d10af96608e10d4bdc
sh <16303791+speedyhoon@users.noreply.github.com>
2016-05-06 08:41:23 +1000

    More changes to EventBucket sub directory that wasn't committed

--- js/startShooting.js
+++ js/startShooting.js
@@ -1,32 +1,32 @@
 (function(){
 	'use strict';
 	var currentCell, currentRow, classes = {
-		currentType:null,
-		0:{
-			sighters:2,
-			validShots:'012345V6X',
-			validScore:'012345555',
-			validCenta:'000000111',
-			validSighters:'abcdefvvx',
-			buttons:'012345VX'
-		},
-		1:{
-			sighters:3,
-			validShots:'012345V6X',
-			validScore:'012345666',
-			validCenta:'000000001',
-			validSighters:'abcdefggx',
-			buttons:'0123456X'
-		},
-		2:{
-			sighters:2,
-			validShots:'012345V6X',
-			validScore:'012345555',
-			validCenta:'000000111',
-			validSighters:'abcdefvvx',
-			buttons:'012345VX'
-		}
-	};
+			currentType:null,
+			0:{
+				sighters:2,
+				validShots:'012345V6X',
+				validScore:'012345555',
+				validCenta:'000000111',
+				validSighters:'abcdefvvx',
+				buttons:'012345VX'
+			},
+			1:{
+				sighters:3,
+				validShots:'012345V6X',
+				validScore:'012345666',
+				validCenta:'000000001',
+				validSighters:'abcdefggx',
+				buttons:'0123456X'
+			},
+			2:{
+				sighters:2,
+				validShots:'012345V6X',
+				validScore:'012345555',
+				validCenta:'000000111',
+				validSighters:'abcdefvvx',
+				buttons:'012345VX'
+			}
+		};
 	if(window.XMLHttpRequest){
 		var j = new XMLHttpRequest();
 	}
@@ -44,26 +44,30 @@
 
 	function getValue(thisCell, attribute){
 		//Putting plus in front of a string converts it to a number (integer or float)
-		return +thisCell.getAttribute('data-' + attribute);
+		return+thisCell.getAttribute('data-' + attribute);
 	}
 
 	function recalculateTotal(value){
 		var type = getShootersClass(), index = classes[type].validShots.indexOf(value), newer = ~~classes[type].validScore[index], newerC = ~~classes[type].validCenta[index], total, centers;
 		currentCell.textContent = value;
+		console.log(currentRow.total, currentRow.centers, currentCell.value, currentCell.center);
 		if(getCurrentNth() >= getNoOfSighters()){
 			total = getValue(currentRow, 'total') - getValue(currentCell, 'value') + newer;
 			centers = getValue(currentRow, 'centers') - getValue(currentCell, 'center') + newerC;
 			currentRow.lastChild.innerHTML = total + (centers ? '<sup>' + centers + '</sup>' : '');
-			currentRow.setAttribute('data-total', total);
-			currentRow.setAttribute('data-centers', centers);
+//currentRow.setAttribute('data-total', total);
+			currentRow.total = total;
+//currentRow.setAttribute('data-centers', centers);
+			currentRow.centers = centers;
 		}
-		currentCell.setAttribute('data-value', newer);
-		currentCell.setAttribute('data-center', newerC);
+//currentCell.setAttribute('data-value', newer);
+		currentCell.value = newer;
+		currentCell.center = newerC;
 	}
 
 	function getShots(){
-		var cells = currentRow.getElementsByTagName('td'), shootersClass = classes[getShootersClass()], send = '', sighters = getNoOfSighters(), value = '', i=-1, max = cells.length, index;
-		while(++i<max){
+		var cells = currentRow.getElementsByTagName('td'), shootersClass = classes[getShootersClass()], send = '', sighters = getNoOfSighters(), value = '', i = -1, max = cells.length, index;
+		while(++i < max){
 			value = cells[i].textContent;
 			if(!value){
 				send += '-';
@@ -113,7 +117,7 @@
 		if(currentCell){
 			if(currentCell.textContent !== value){//prevents recalculating the score if it is the same value
 				recalculateTotal(value);
-				ajax(currentRow.getAttribute('id'));
+				ajax(currentRow.id);
 			}
 			if(currentCell.nextSibling && currentCell.nextSibling.nodeName === 'TD'){
 				highlightOnlyTheCell(currentCell.nextSibling);
@@ -125,11 +129,11 @@
 		var type = getShootersClass();
 		if(type && classes.currentType !== type && classes[type].buttons){
 			classes.currentType = type;
-			var h=-1, td = document.createElement('td'), buttonLength = classes[type].buttons.length, buttonOnClickEvent = function buttonClickEventer(buttonValue){
-				return function buttonClicker(){
-					changeValue(buttonValue);
+			var h = -1, td = document.createElement('td'), buttonLength = classes[type].buttons.length, buttonOnClickEvent = function buttonClickEventer(buttonValue){
+					return function buttonClicker(){
+						changeValue(buttonValue);
+					};
 				};
-			};
 			while(++h < buttonLength){
 				var button = document.createElement('button');
 				button.textContent = classes[type].buttons[h];
@@ -137,7 +141,7 @@
 				td.appendChild(button);
 			}
 			var buttonsCell = document.getElementById('bu');
-			td.id='bu';
+			td.id = 'bu';
 			td.setAttribute('colspan', buttonsCell.getAttribute('colspan'));
 			buttonsCell.parentNode.replaceChild(td, buttonsCell);
 		}
@@ -149,7 +153,7 @@
 		generateButtons();
 	}
 
-	/*	function changeSighters(){//when changing the value of the select box "sighters"
+	/*function changeSighters(){//when changing the value of the select box "sighters"
 		var selected = document.getElementById('selectSighters').selectedIndex, tds, iteration = 0, sighters, selectedCell, i;
 		if(currentRow && selected > 0){
 			tds = currentRow.getElementsByTagName('td');
@@ -196,6 +200,7 @@
 	}
 
 	function highlightCell(cell){//change the selected table cell (td) to the currentCell selected
+		console.log('highlightCell');
 		if(cell !== currentCell){
 			highlightOnlyTheCell(cell);
 			if(currentRow !== currentCell.parentNode){
@@ -205,22 +210,24 @@
 	}
 
 	var shooters = document.querySelectorAll('tbody th:nth-child(4)'), shooterQty = shooters.length, shooterNameOnclick = function(trElement){
-		return function shooterClick(){
-			highlightRow(trElement);
+			return function shooterClick(){
+				highlightRow(trElement);
 
 			//if visited attribute is present it has already been processed
-			if(!currentRow.getAttribute('data-visited')){
-				currentRow.onclick = function tdClicker(tdElement){
-					return function tdClick(event){
-						if(event.target.nodeName === 'TD'){
+				console.log('visited?', currentRow.visited);
+				if(!currentRow.visited){
+					currentRow.onclick = function tdClicker(tdElement){
+						return function tdClick(event){
+							console.log('tdClicker');
+							if(event.target.nodeName === 'TD'){
 							highlightCell(tdElement);
 						}
+						};
 					};
-				};
-				currentRow.setAttribute('data-visited', 1);
-			}
+					currentRow.visited = 1;
+				}else{console.log('row already visited');}
+			};
 		};
-	};
 	while(shooterQty--){		//assign onclick events to all shooters names
 		shooters[shooterQty].onclick = shooterNameOnclick(shooters[shooterQty].parentNode);
 	}
