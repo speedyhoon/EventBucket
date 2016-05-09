6602dd75401d7161fa47e3b8b8298fc2bda9ece2
sh <16303791+speedyhoon@users.noreply.github.com>
2016-05-09 22:16:18 +1000

    code lint

--- EventBucket/http.go
+++ EventBucket/http.go
@@ -23,8 +23,8 @@ func serveFile(fileName string) {
 	http.HandleFunc("/"+fileName, func(w http.ResponseWriter, r *http.Request) {
 		// Check if the request contains accept gzip encoding header & return the appropriate resource
 		// Unfortunately uncompressed responses may still be required even though all modern browsers support gzip
-		//webmasters.stackexchange.com/questions/22217/which-browsers-handle-content-encoding-gzip-and-which-of-them-has-any-special
-		//www.stevesouders.com/blog/2009/11/11/whos-not-getting-gzip/
+		// webmasters.stackexchange.com/questions/22217/which-browsers-handle-content-encoding-gzip-and-which-of-them-has-any-special
+		// www.stevesouders.com/blog/2009/11/11/whos-not-getting-gzip/
 		headers(w, cache)
 		http.ServeFile(w, r, dirRoot+fileName)
 	})
@@ -33,7 +33,7 @@ func serveFile(fileName string) {
 func serveDir(contentType, gzipDir string) {
 	http.Handle(contentType,
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			//If url is a directory return a 404 to prevent displaying a directory listing.
+			// If url is a directory return a 404 to prevent displaying a directory listing.
 			if strings.HasSuffix(r.URL.Path, "/") {
 				http.NotFound(w, r)
 				return
@@ -56,12 +56,12 @@ var headerOptions = map[string][2]string{
 	dirJS:  {contentType, "text/javascript"},
 	dirPNG: {contentType, "image/png"},
 	dirSVG: {contentType, "image/svg+xml"},
-	//dirWOF2:   {contentType, "application/font-woff2"},
+	// dirWOF2:   {contentType, "application/font-woff2"},
 }
 
-//security add Access-Control-Allow-Origin //net.tutsplus.com/tutorials/client-side-security-best-practices/
+// security add Access-Control-Allow-Origin // net.tutsplus.com/tutorials/client-side-security-best-practices/
 func headers(w http.ResponseWriter, setHeaders ...string) {
-	//The page cannot be displayed in a frame, regardless of the site attempting to do so. //developer.mozilla.org/en-US/docs/Web/HTTP/X-Frame-Options
+	// The page cannot be displayed in a frame, regardless of the site attempting to do so. // developer.mozilla.org/en-US/docs/Web/HTTP/X-Frame-Options
 	w.Header().Set("X-Frame-Options", "DENY")
 	for _, lookup := range setHeaders {
 		switch lookup {
@@ -76,7 +76,7 @@ func headers(w http.ResponseWriter, setHeaders ...string) {
 			w.Header().Set("Pragma", "no-cache")
 			break
 		default:
-			//TODO add comment
+			// TODO add comment
 			if lookup == cGzip || headerOptions[lookup][0] == "Content-Type" {
 				w.Header().Set(headerOptions[lookup][0], headerOptions[lookup][1])
 			}
@@ -98,14 +98,14 @@ func get404(url string, pageFunc func(http.ResponseWriter, *http.Request)) {
 func getRedirectPermanent(url string, pageFunc func(http.ResponseWriter, *http.Request)) {
 	http.HandleFunc(url,
 		func(w http.ResponseWriter, r *http.Request) {
-			//Don't accept post or put requests
+			// Don't accept post or put requests
 			if r.Method != get {
 				http.Redirect(w, r, url, http.StatusSeeOther)
 			}
 			pageFunc(w, r)
 		})
-	//Redirects back to subdirectory "url". Needed when url parameters are not wanted or needed.
-	//e.g. if url = "foobar" then "http://localhost/foobar/fdsa" will redirect to "http://localhost/foobar"
+	// Redirects back to subdirectory "url". Needed when url parameters are not wanted or needed.
+	// e.g. if url = "foobar" then "http:// localhost/foobar/fdsa" will redirect to "http:// localhost/foobar"
 	http.Handle(url+"/", http.RedirectHandler(url, http.StatusMovedPermanently))
 }
 
@@ -117,7 +117,7 @@ func getParameters(url string, pageFunc func(http.ResponseWriter, *http.Request,
 	var parameters, lowerParams string
 	http.HandleFunc(url,
 		func(w http.ResponseWriter, r *http.Request) {
-			//Don't accept post or put requests
+			// Don't accept post or put requests
 			if r.Method != get {
 				http.Redirect(w, r, url, http.StatusSeeOther)
 			}
@@ -126,7 +126,7 @@ func getParameters(url string, pageFunc func(http.ResponseWriter, *http.Request,
 			lowerParams = strings.ToLower(parameters)
 
 			if parameters != lowerParams {
-				//Redirect to page with lowercase parameters.
+				// Redirect to page with lowercase parameters.
 				http.Redirect(w, r, url+lowerParams, http.StatusSeeOther)
 				return
 			}
@@ -140,17 +140,17 @@ func getParameters(url string, pageFunc func(http.ResponseWriter, *http.Request,
 }
 
 func errorHandler(w http.ResponseWriter, r *http.Request, status int, errorType string) {
-	//All EventBucket page urls and ids are lowercase
+	// All EventBucket page urls and ids are lowercase
 	lowerURL := strings.ToLower(strings.TrimSuffix(r.URL.Path, "/"))
 
-	//prevents a redirect loop if url is already in lowercase letters.
+	// prevents a redirect loop if url is already in lowercase letters.
 	if r.URL.Path != lowerURL {
 
-		//check if the request matches any of the pages that don't require parameters
+		// check if the request matches any of the pages that don't require parameters
 		if strings.Count(lowerURL, "/") >= 2 {
 			for _, page := range []string{urlAbout, urlArchive, urlClubs /*urlEvent,*/, urlLicence, urlShooters} {
 				if strings.HasPrefix(lowerURL, page) {
-					//redirect to page without parameters
+					// redirect to page without parameters
 					http.Redirect(w, r, page, http.StatusSeeOther)
 					return
 				}
@@ -168,9 +168,9 @@ func errorHandler(w http.ResponseWriter, r *http.Request, status int, errorType 
 	})
 }
 
-//whoops an error occured
+// whoops an error occurred
 // that club id you supplied doesn't match anything
-//here is a list of valid clubs - that link to the clubsettings page.
+// here is a list of valid clubs - that link to the clubsettings page.
 func whoops(w http.ResponseWriter, r *http.Request, url string) {
 	var pageName string
 	pageType := "event"
--- EventBucket/j/shooterSearch
+++ EventBucket/j/shooterSearch
@@ -1,23 +1,23 @@
 'use strict';
 var xhr = new XMLHttpRequest, f = '', s = '', c = '';
-function getter(form) {
-  //Ignore form inputs into other fields
-  if (f === form.f.value && s === form.s.value && c === form.C.value) return;
-  f = form.f.value;
-  s = form.s.value;
-  c = form.C.value;
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      form.S.removeAttribute('class');
-      if (xhr.status == 200 && xhr.responseText.length) {
-        form.S.innerHTML = xhr.responseText;
-        form.S.parentElement.removeAttribute('hidden');
-      }else {
-        form.S.parentElement.setAttribute('hidden','');
-      }
-    }
-  };
-  form.S.setAttribute('class', 'loading');
-  xhr.open('GET', '/9' + (f ? '?f=' + f : '') + (s ? '?s=' + s : '') + (c ? '?c=' + c : ''), true);
-  xhr.send();
+function getter(form){
+	//Ignore form inputs into other fields
+	if(f === form.f.value && s === form.s.value && c === form.C.value)return;
+	f = form.f.value;
+	s = form.s.value;
+	c = form.C.value;
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState == 4){
+			form.S.removeAttribute('class');
+			if(xhr.status == 200 && xhr.responseText.length){
+				form.S.innerHTML = xhr.responseText;
+				form.S.parentElement.removeAttribute('hidden');
+			}else{
+				form.S.parentElement.setAttribute('hidden', '');
+			}
+		}
+	};
+	form.S.setAttribute('class', 'loading');
+	xhr.open('GET', '/9' + (f ? '?f=' + f : '') + (s ? '?s=' + s : '') + (c ? '?c=' + c : ''), true);
+	xhr.send();
 }
