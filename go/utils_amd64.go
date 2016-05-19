ff2e2935d496128c181bf5ebfbd2320ab6e71445
Cam <eventbucket@hotmail.com>
2016-05-19 21:41:08 +1000

    rename strToUint to stoU

--- go/utils_amd64.go
+++ go/utils_amd64.go
@@ -15,8 +15,7 @@ func b36tou(id string) (uint, error) {
 }
 
 //Converts numeric string to uint
-//TODO rename stoU
-func strToUint(id string) (uint, error) {
+func stoU(id string) (uint, error) {
 	u, err := strconv.ParseUint(strings.TrimSpace(id), 10, 64)
 	if err != nil {
 		warn.Printf("Unable to convert %v to uint", id)
