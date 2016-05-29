cb388c57d27b636c25b411d6cba814f885c69c6a
sh <16303791+speedyhoon@users.noreply.github.com>
2016-05-29 19:44:04 +1000

    merge table editRow code into one file and call one function buildRow per page. Code is now DRY!

--- js/entries.js
+++ js/entries.js
@@ -1,48 +1,15 @@
 'use strict';
-var editRows = document.querySelectorAll('#editRow tbody tr td:last-of-type'), i = editRows.length;
+var button = document.querySelectorAll('[action="/8"] button'), i=button.length;
 while(i--){
-	editRows[i].onclick = editRow;
+	button[i].onclick = function shooterEntry(){
+		var isNew = !this.getAttribute('formAction');
+		this.form.f.required = isNew;
+		this.form.s.required = isNew;
+		this.form.S.required = !isNew;
+		this.form.C.required = isNew;
+	};
 }
-function editRow(editCell){
-	var row = editCell.target.parentElement,
-		tds = row.children,
-		t = document.importNode(document.querySelector('template').content, true),
-		form = t.querySelector('form'),
-		outerFormFields = t.querySelectorAll('[form=editRow]'),
-		i = outerFormFields.length;
-	form.id = '_' + tds[0].textContent;
-	while(i--){
-		outerFormFields[i].setAttribute('form', form.id);
-	}
-	t.querySelector('td').textContent = tds[0].textContent;
-
-	t = entries(t, tds);
-	row.innerHTML = '';
-	row.appendChild(t);
-}
-function findValues(element, labels){
-	var i = labels.length;
-	while(i--){
-		findValue(element, labels[i]);
-	}
-}
-function findValue(element, label){
-	var i = element.options.length;
-	while(i--){
-		//Select the option if its text = the label. Trim isn't required because the server outputs html without any wrapping whitespace.
-		if(label === element.options[i].textContent){
-			//Assign the selected option and exit the loop
-			return element.options[i].setAttribute('selected', '');
-		}
-	}
-}
-function checked(cell, element){
-	if(cell.className === '^tick^'){
-		element.setAttribute('checked', '');
-	}
-}
-
-function entries(t, tds){
+function buildRow(t, tds){
 	var surname = tds[1].querySelector('span');
 	t.querySelector('[name=s]').value = surname.textContent;
 	surname.textContent = '';
@@ -54,14 +21,4 @@ function entries(t, tds){
 	checked(tds[5], t.querySelector('[name=k]'));
 	checked(tds[6], t.querySelector('[name=x]'));
 	return t;
-}
-var button = document.querySelectorAll('[action="/8"] button'), i=button.length;
-while(i--){
-	button[i].onclick = function shooterEntry(){
-		var isNew = !this.getAttribute('formAction');
-		this.form.f.required = isNew;
-		this.form.s.required = isNew;
-		this.form.S.required = !isNew;
-		this.form.C.required = isNew;
-	};
 }
--- js/eventRanges.js
+++ js/eventRanges.js
@@ -1,14 +1,5 @@
 'use strict';
-var editShooters = document.querySelectorAll('#editRow tbody tr td:last-of-type'), qty = editShooters.length;
-while(qty--){
-	editShooters[qty].onclick = stuff;
-}
-function stuff(editCell){
-	var row = editCell.target.parentElement;
-	var tds = row.children, t = document.importNode(document.querySelector('template').content, true), shooterID = '_' + tds[0].textContent;
-	t.querySelector('form').id = shooterID;
-	t.querySelector('td').textContent = tds[0].textContent;
-	t.querySelector('[name=n]').value = tds[1].textContent;
+function buildRow(t, tds){
 	var R = t.querySelector('[name=R]');
 	if(tds[2].textContent){
 		findValues(R, tds[2].textContent.replace(/, $/, '').split(', '));
@@ -16,7 +7,6 @@ function stuff(editCell){
 	}else{
 		R.parentElement.removeChild(R);
 	}
-
 	var l = t.querySelector('[name=k]');
 	if(!tds[2].textContent){
 		if(tds[3].className == '^tick^'){
@@ -26,26 +16,5 @@ function stuff(editCell){
 		l.parentElement.removeChild(l);
 	}
 	t.querySelector('[name=I]').value = tds[0].textContent;
-	var outerFormFields = t.querySelectorAll('[form=editRow]'), index = outerFormFields.length;
-	while(index--){
-		outerFormFields[index].setAttribute('form', shooterID);
-	}
-	row.innerHTML = '';
-	row.appendChild(t);
-}
-function findValues(element, labels){
-	var i = labels.length;
-	while(i--){
-		findValue(element, labels[i]);
-	}
-}
-function findValue(element, label){
-	var i = element.options.length;
-	while(i--){
-		//Select the option if its text = the label. Trim isn't required because the server outputs html without any wrapping whitespace.
-		if(label === element.options[i].textContent){
-			//Assign the selected option and exit the loop
-			return element.options[i].setAttribute('selected', '');
-		}
-	}
+	return t;
 }
