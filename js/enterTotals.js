a5ff72df2d61d8318e1731ce0687995e3a03a439
sh <16303791+speedyhoon@users.noreply.github.com>
2016-06-03 08:40:09 +1000

    return save confirmation and failure to save.

--- js/enterTotals.js
+++ js/enterTotals.js
@@ -22,7 +22,9 @@ function reconnect (){
 	};
 	//TODO
 	//Update UI with save / error message.
-	//ws.onmessage = function(message){};
+	ws.onmessage = function(message){
+		console.log(message.data);
+	};
 	ws.onclose = function(){
 		if(!intervalId){
 			intervalId = setInterval(reconnect, 3000); //try to reconnect every 3 seconds after the connection is dropped.
