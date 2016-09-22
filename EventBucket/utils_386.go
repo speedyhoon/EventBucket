ca7601626f72bd6b5e3bdbe0c055a43453302376
sh <16303791+speedyhoon@users.noreply.github.com>
2016-09-22 12:04:20 +1000

    Remove unique code for _386

--- EventBucket/utils_386.go
+++ EventBucket/utils_386.go
@@ -1,15 +1,3 @@
 package main
 
-import (
-	"strconv"
-	"strings"
-)
-
-//Converts numeric string to uint
-func stoU(id string) (uint, error) {
-	u, err := strconv.ParseUint(strings.TrimSpace(id), 10, 32)
-	if err != nil {
-		warn.Printf("Unable to convert %v to uint", id)
-	}
-	return uint(u), err
-}
+const systemArch = 32
