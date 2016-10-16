2e7c7ed7b5c8d6247df925835188b905944eb833
sh <16303791+speedyhoon@users.noreply.github.com>
2016-10-16 14:50:41 +1100

    Change stoU function to be system independent

---
+++ go/utils.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+//Converts numeric string to uint
+func stoU(id string) (uint, error) {
+	u, err := strconv.ParseUint(strings.TrimSpace(id), 10, sysArch)
+	if err != nil {
+		warn.Printf("Unable to convert %v to uint", id)
+	}
+	return uint(u), err
+}
