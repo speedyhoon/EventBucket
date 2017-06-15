42a3c16f6958bfee68fe8193f72a70f5b4e5e4dc
sh <16303791+speedyhoon@users.noreply.github.com>
2017-06-15 17:56:07 +1000

    Fix port number Improve comments

--- go/pSettings.go
+++ go/pSettings.go
@@ -7,12 +7,10 @@ import (
 func settings(w http.ResponseWriter, r *http.Request) {
 	templater(w, page{
 		Title: "Settings",
-
 		Data: map[string]interface{}{
-			//Form 1: update port and/or theme
-			"Port":  portAddr + "--BuildDate GoVersion f fdsasds5", //during save, change post redirect to new port address
+			"Port":  portAddr, //TODO during save, redirect after post request to the new port address
 			"Theme": masterTemplate.Theme,
 		},
-		//TODO Form 2: shutdown http server
+		//TODO Add form 2 for shutting down the HTTP server
 	})
 }
