090b89c148712c5de9e23735f541d91b71f91bf2
sh <16303791+speedyhoon@users.noreply.github.com>
2017-06-22 06:50:03 +1000

    Add go1.8 signal to gracefully stop http server Move init functions & global vars into files that they are dependant on Remove masterTemplate variables

--- go/main.go
+++ go/main.go
@@ -12,45 +12,26 @@ import (
 	"os"
 	"path/filepath"
 
+	"context"
+	"os/signal"
+	"time"
+
 	"github.com/boltdb/bolt"
 )
 
 var (
-	//Database connection.
-	db *bolt.DB
-
 	//Command line flags.
 	portAddr, dbPath string
 	debug            bool
 
-	runDir string
-
 	//Logging
+	//TODO add t & info when debug == true during build time
 	t    = log.New(ioutil.Discard, "TRACE: ", log.Lshortfile) //Flags can be log.Lshortfile|log.Ltime
 	info = log.New(os.Stdout, "", 0)
 	warn = log.New(os.Stderr, "WARN: ", log.Lshortfile)
 )
 
 func init() {
-	go maintainSessions()
-	go maintainExpires()
-
-	var err error
-	runDir, err = os.Executable()
-	if err == nil {
-		runDir = filepath.Dir(runDir)
-		htmlDirectory = filepath.Join(runDir, "h")
-	}
-	masterTemplatePath = filepath.Join(htmlDirectory, "!")
-	masterScoreboard = filepath.Join(htmlDirectory, "masterscoreboard")
-	formsTemplatePath = filepath.Join(htmlDirectory, "forms")
-	reusablesTemplatePath = filepath.Join(htmlDirectory, "reusables")
-
-	masterStuff = [][]string{
-		{formsTemplatePath, reusablesTemplatePath, masterTemplatePath},
-		{masterScoreboard},
-	}
-
 	//Command line flags
 	flag.StringVar(&dbPath, "dbpath", filepath.Join(os.Getenv("ProgramData"), `EventBucket`), "Directory for datafiles.")
 	flag.BoolVar(&debug, "debug", false, "Turn on debugging and turn off HTML file caching & club maps.")
@@ -60,7 +41,7 @@ func init() {
 	flag.Parse()
 
 	//Create database directory if needed.
-	err = mkDir(dbPath)
+	err := mkDir(dbPath)
 	if err != nil {
 		warn.Println(err)
 		os.Exit(1)
@@ -70,7 +51,7 @@ func init() {
 		t.SetOutput(os.Stdout)
 	}
 
-	//Try to load the grades file if any is specified
+	//Try to load the grades file if any are specified
 	if loadGrades(*gradesFilePath) != nil {
 		redoGlobals([]Discipline{})
 		//If a file path was specified try to create one
@@ -91,39 +72,36 @@ func init() {
 }
 
 func main() {
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
 	//Database save location
 	dbPath = filepath.Join(dbPath, "EventBucket.db")
-	info.Println("Opening database...", dbPath)
+
 	var err error
-	db, err = bolt.Open(dbPath, 0644, nil)
+	db, err = bolt.Open(dbPath, 0644, &bolt.Options{Timeout: 8 * time.Second, InitialMmapSize: 1048576})
 	if err != nil {
-		warn.Println(err)
-		//TODO add db.Close during or before http server shutdown when go1.8 is released
-		//db.Close()
+		warn.Println("Connection timeout. Unable to open", dbPath)
 		os.Exit(4)
 	}
 	defer db.Close()
+
 	//Prepare database by creating all buckets (tables) needed. Otherwise view (read only) transactions will fail.
 	makeBuckets()
 
-	pages()
 	info.Print("Starting EventBucket HTTP server...")
 
-	if !debug {
-		openBrowser(portAddr)
-	}
-	warn.Printf("ListenAndServe: %v", http.ListenAndServe(portAddr, nil))
-	info.Println("EvenBucket server stopped.")
-}
-
-//Attempt to create the path supplied if it doesn't exist.
-func mkDir(path string) error {
-	info, err := os.Stat(path)
-	if err != nil || !info.IsDir() {
-		err = os.Mkdir(path, os.ModeDir)
-		if err != nil {
-			warn.Printf("Unable to create directory %v %v", path, err)
+	h := http.Server{Addr: portAddr, Handler: nil}
+	go func() {
+		if err := h.ListenAndServe(); err != nil {
+			warn.Fatal(err)
+		} else if !debug {
+			openBrowser("http://localhost" + portAddr)
 		}
-	}
-	return err
+	}()
+
+	<-stop
+	info.Println("Shutting down the server...")
+	h.Shutdown(context.Background())
+	info.Println("EvenBucket server stopped.")
 }
