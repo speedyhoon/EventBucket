694849c5928fc84eb36fca95c6fb47114b78bdbf
Cam <info@eventbucket.com>
2017-06-30 00:12:41 +1000

    Change cmdline flag to accept host address & port no. Change openBrowser to be more generic

--- go/utils.go
+++ go/utils.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"os"
+	"os/exec"
+	"runtime"
 	"strconv"
 	"strings"
 )
@@ -38,4 +40,21 @@ func mkDir(path string) error {
 		}
 	}
 	return err
+}
+
+// startBrowser tries to open the URL in a browser, and returns whether it succeed.
+func openBrowser(url string) bool {
+	info.Println("openBrowser")
+	// try to start the browser
+	var args []string
+	switch runtime.GOOS {
+	case "darwin":
+		args = []string{"open"}
+	case "windows":
+		args = []string{"cmd", "/c", "start"}
+	default:
+		args = []string{"xdg-open"}
+	}
+	cmd := exec.Command(args[0], append(args[1:], url)...)
+	return cmd.Start() == nil
 }
