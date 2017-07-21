f1f40bd05db9d471f7e2559b357204016d7d8ef3
sh <16303791+speedyhoon@users.noreply.github.com>
2017-07-21 19:51:33 +1000

    move about page into a separate file

---
+++ go/pAbout.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func about(w http.ResponseWriter, r *http.Request) {
+	templater(w, page{
+		Title: "About",
+		Data:  localIPs(),
+	})
+}
+
+//localIPs returns the non loopback local IPv4 of the host
+func localIPs() map[string]interface{} {
+	var localIPs []string
+	addrs, err := net.InterfaceAddrs()
+	if err == nil {
+		var ipnet *net.IPNet
+		var ok bool
+		for _, address := range addrs {
+			//Check the address type is not localhost or a loopback address
+			ipnet, ok = address.(*net.IPNet)
+			if ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil && !strings.HasPrefix(ipnet.IP.String(), "169.254.") {
+				localIPs = append(localIPs, ipnet.IP.String()+portAddr)
+			}
+		}
+	}
+	hostname, _ := os.Hostname()
+	return map[string]interface{}{
+		"hostname":    hostname + portAddr,
+		"ipAddresses": localIPs,
+	}
+}
--- go/pEvents.go
+++ go/pEvents.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"net"
 	"net/http"
-	"os"
 	"sort"
 	"strings"
 	"time"
@@ -26,35 +24,6 @@ func events(w http.ResponseWriter, r *http.Request) {
 			"Network":  localIPs(),
 		},
 	})
-}
-
-func about(w http.ResponseWriter, r *http.Request) {
-	templater(w, page{
-		Title: "About",
-		Data:  localIPs(),
-	})
-}
-
-//localIPs returns the non loopback local IPv4 of the host
-func localIPs() map[string]interface{} {
-	var localIPs []string
-	addrs, err := net.InterfaceAddrs()
-	if err == nil {
-		var ipnet *net.IPNet
-		var ok bool
-		for _, address := range addrs {
-			//Check the address type is not localhost or a loopback address
-			ipnet, ok = address.(*net.IPNet)
-			if ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil && !strings.HasPrefix(ipnet.IP.String(), "169.254.") {
-				localIPs = append(localIPs, ipnet.IP.String()+portAddr)
-			}
-		}
-	}
-	hostname, _ := os.Hostname()
-	return map[string]interface{}{
-		"hostname":    hostname + portAddr,
-		"ipAddresses": localIPs,
-	}
 }
 
 //CalendarEvent is the same as Event struct without Shooters and their scores.
