732748a918e3013a68c6fdc42194d5687bb38755
Cam <info@eventbucket.com>
2017-07-25 22:05:40 +1000

    prefix element variables with $ reuse existing declared variables create dialog element when needed, not during page load

--- js/main.js
+++ js/main.js
@@ -1,89 +1,93 @@
 //Prevent users from double/triple clicking on submit buttons that could cause a form to submit more than once with exactly the same data. If forms used a CSRF token this JavaScript would be redundant
-var submitted = false, n = document.forms.length;
+var submitted = 0, i = document.forms.length;
 function stopDoubleSubmit(){
-	if(submitted)return false;
-	return submitted = true;
+	if(submitted)return 0;
+	return submitted = 1;
 }
 
-while(n--){
-	if(!document.forms[n].onsubmit){
-		document.forms[n].onsubmit = stopDoubleSubmit;
+while(i--){
+	if(!document.forms[i].onsubmit){
+		document.forms[i].onsubmit = stopDoubleSubmit;
 	}
 }
 
-function tableSort(th){
-	if(th.parentElement.parentElement.nodeName !== 'THEAD'){
-		return;
-	}
-	//If th.textContent == id compare using base36 else use textContent
-	var tbody = th.parentElement.parentElement.parentElement.querySelector('tbody'),
-		column = Array.prototype.indexOf.call(th.parentElement.children, th),
-		direction = th.className === '^asc^' ? -1 : 1,
-		rows = Array.from(tbody.children);
-	var sortBy = function(cell){
-		if(!cell){
-			return '';
-		}
-		var input = cell.textContent;
-		switch(th.textContent){
-		case 'ID':
-			return ~~input;
-			//Used on the Shooters page
-		case 'Id':
-			return parseInt(input, 36);   //Id = base 36 string (0-9a-z) e.g. a2e = 13046
-		}
-		return input;
-	};
-	rows.sort(function(a, b){
-		a = sortBy(a.children[column]);
-		b = sortBy(b.children[column]);
-		return a > b ? direction : a < b ? -1 * direction : 0;
-	});
-	tbody.innerHTML = '';
-	for(var i = 0, max = rows.length; i < max; i++){
-		tbody.appendChild(rows[i]);
-	}
-	var ths = th.parentElement.querySelectorAll('.^asc^,.^desc^'), qty = ths.length;
-	while(qty--){
-		ths[qty].removeAttribute('class');
-	}
-	th.className = direction > 0 ? '^asc^' : '^desc^';
-}
+var $dialog, $label;
 
-//Form help dialog popup
-var dialog = document.createElement('dialog'), label;
-dialog.onclick = function(){
-	dialog.close();
-	if(label){
-		label.click();
-		label = 0;
-	}
-};
-document.body.appendChild(dialog);
-
-//Add table sort to TH elements within a THEAD
 document.onclick = function(event){
-	var target = event.target;
-	switch(target.nodeName){
+	switch(event.target.nodeName){
 	//Help message pop-up
 	case 'ABBR':
-		dialog.textContent = target.title;
+		if(!$dialog){
+			//Form help dialog popup
+			$dialog = document.createElement('dialog');
+			$dialog.onclick = function(){
+				$dialog.open = 0;
+				//If label is set, click on the parent label element
+				if($label){
+					$label.click();
+					$label = 0;
+				}
+			};
+			document.body.appendChild($dialog);
+		}
+		$dialog.textContent = event.target.title;
 		//Assuming the <abbr> element will always be the immediate child of a <label> element
-		label = target.parentElement;
-		dialog.showModal();
+		$label = event.target.parentElement;
+		$dialog.open = 1;
 		break;
 	//Table sort
 	case 'TH':
-		tableSort(target);
+		tableSort(event.target);
 	}
 };
 
 //Start event listeners that add a class to form fields based on their valid/invalid values that changes the background colour.
-var inputs = document.querySelectorAll('input,select'), i = inputs.length, flagClass = function(event){
+var inputs = document.querySelectorAll('input,select'), flagClass = function(event){
 		event.srcElement.classList.toggle('^dirty^', !event.srcElement.validity.valid);
 	};
+i = inputs.length;
 while(i--){
 	inputs[i].addEventListener('blur', flagClass);
 	inputs[i].addEventListener('invalid', flagClass);
 	inputs[i].addEventListener('input', flagClass);
+}
+
+//Add table sort to <th> elements within a <thead>
+function tableSort($th){
+	if($th.parentElement.parentElement.nodeName !== 'THEAD'){
+		return;
+	}
+	//If th.textContent == id compare using base36 else use textContent
+	var $tbody = $th.parentElement.parentElement.parentElement.querySelector('tbody'),
+		column = Array.prototype.indexOf.call($th.parentElement.children, $th),
+		direction = $th.className === '^asc^' ? 1 : -1,
+		$rows = Array.from($tbody.children);
+	var sortBy = function($cell){
+		if(!$cell){
+			return '';
+		}
+		switch($th.textContent){
+			//Numeric integer identifier
+			case 'ID':
+				return ~~$cell.textContent;
+			//Base 36 [0-9a-z] identifier string e.g. a2e = 13046. Used on the Shooters page
+			case 'Id':
+				return parseInt($cell.textContent, 36);
+		}
+		return $cell.textContent;
+	};
+	$rows.sort(function(a, b){
+		a = sortBy(a.children[column]);
+		b = sortBy(b.children[column]);
+		return a > b ? direction : a < b ? -1 * direction : 0;
+	});
+	$tbody.innerHTML = '';
+	for(var i = 0, max = $rows.length; i < max; i++){
+		$tbody.appendChild($rows[i]);
+	}
+	var $ths = $th.parentElement.querySelectorAll('.^asc^,.^desc^'), qty = $ths.length;
+	while(qty--){
+		$ths[qty].removeAttribute('class');
+	}
+	$th.className = direction < 0 ? '^asc^' : '^desc^';
 }
