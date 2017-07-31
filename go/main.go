e8ac07c090b7dd39e5c1c11c56f2243162fb4349
Cam <info@eventbucket.com>
2017-07-31 18:50:04 +1000

    Reduce the number of global variables debug is no longer a *bool only return []option from searchShooters database call

--- go/main.go
+++ go/main.go
@@ -20,10 +20,8 @@ import (
 
 var (
 	//Command line flags.
-	portAddr   = ":"
-	dbPath     = flag.String("dbpath", filepath.Join(os.Getenv("ProgramData"), "EventBucket"), "Directory for datafiles.")
-	debug      = flag.Bool("debug", false, "Turn on debugging and turn off club maps.")
-	httpListen = flag.String("http", "127.0.0.1:80", "host:port to listen on")
+	portAddr = ":"
+	debug    bool
 
 	//Logging
 	//TODO add t & info when debug == true during build time
@@ -32,39 +30,17 @@ var (
 	warn = log.New(os.Stderr, "WARN: ", log.Lshortfile)
 )
 
-func init() {
+func main() {
 	//Command line flags
-	flag.BoolVar(&masterTemplate.Theme, "dark", false, "Switch EventBucket to use a dark theme for night shooting")
+	flag.BoolVar(&masterTemplate.IsDarkTheme, "dark", false, "Switch EventBucket to use a dark theme for night shooting")
+	flag.BoolVar(&debug, "debug", false, "Turn on debugging and turn off club maps.")
 	gradesFilePath := flag.String("grades", "", "Load grade settings from a JSON file. If the file doesn't exist, EventBucket will try to create it & exit")
+	httpListen := flag.String("http", "127.0.0.1:80", "host:port to listen on")
+	dbPath := flag.String("dbpath", filepath.Join(os.Getenv("ProgramData"), "EventBucket"), "Directory for datafiles.")
 	flag.Parse()
 
 	//Create database directory if needed.
 	err := mkDir(*dbPath)
-	if err != nil {
-		warn.Fatal(err)
-	}
-
-	if *debug {
-		t.SetOutput(os.Stdout)
-	}
-
-	//Try to load the grades file if any are specified
-	if loadGrades(*gradesFilePath) != nil {
-		redoGlobals([]Discipline{})
-		//If a file path was specified try to create one
-		if *gradesFilePath != "" {
-			buildGradesFile(*gradesFilePath)
-			os.Exit(2)
-		}
-	}
-}
-
-func main() {
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt)
-
-	//HTTP server host & port
-	host, port, err := net.SplitHostPort(*httpListen)
 	if err != nil {
 		warn.Fatal(err)
 	}
@@ -84,11 +60,35 @@ func main() {
 	//Prepare database by creating all buckets (tables) needed. Otherwise view (read only) transactions will fail.
 	makeBuckets()
 
+	//Turn on trace logging
+	if debug {
+		t.SetOutput(os.Stdout)
+	}
+
+	//Try to load the grades file if any are specified
+	if loadGrades(*gradesFilePath) != nil {
+		redoGlobals([]Discipline{})
+		//If a file path was specified try to create one
+		if *gradesFilePath != "" {
+			buildGradesFile(*gradesFilePath)
+			os.Exit(2)
+		}
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	//HTTP server host & port
+	host, port, err := net.SplitHostPort(*httpListen)
+	if err != nil {
+		warn.Fatal(err)
+	}
+
 	if host == "" {
 		host = "localhost"
 	}
 	if port != "80" {
-		portAddr = ":" + port
+		portAddr += port
 	}
 	httpAddr := host + portAddr
 	h := http.Server{Addr: httpAddr, Handler: nil}
@@ -98,7 +98,7 @@ func main() {
 		} else {
 			info.Print("Started EventBucket HTTP server...")
 			url := "http://" + httpAddr
-			if !*debug && openBrowser(url) {
+			if !debug && openBrowser(url) {
 				info.Printf("A browser window should open. If not, please visit %s", url)
 			} else {
 				info.Printf("Please open your web browser and visit %s", url)
