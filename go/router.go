b86bca2d3e336506b2c94035f6280da164697b5b
Cam <info@eventbucket.com>
2017-10-09 23:11:08 +1100

    revert router

--- go/router.go
+++ go/router.go
@@ -75,3 +75,100 @@ func post(url string, formID uint8, page func(f form) (string, error)) {
 		func(w http.ResponseWriter, r *http.Request) {
 			if r.Method != "POST" {
 				/*405 Method Not Allowed
+				A request was made of a resource using a request method not supported by that resource; for example,
+				using GET on a form which requires data to be presented via POST, or using POST on a read-only resource.
+				//en.wikipedia.org/wiki/List_of_HTTP_status_codes*/
+				http.Redirect(w, r, r.Referer(), http.StatusMethodNotAllowed)
+				return
+			}
+			f, ok := validBoth(r, formID)
+			if !ok {
+				setSession(w, f)
+				http.Redirect(w, r, r.Referer(), http.StatusSeeOther)
+				return
+			}
+			redirect, err := page(f)
+			//Display any insert errors onscreen.
+			if err != nil {
+				formError(w, r, f, err)
+				return
+			}
+			if redirect == "" {
+				redirect = r.Referer()
+			}
+			http.Redirect(w, r, redirect, http.StatusSeeOther)
+		},
+	)
+}
+
+func get(url string, formID uint8, page func(http.ResponseWriter, *http.Request, form)) {
+	http.HandleFunc(
+		url,
+		isGetMethod(func(w http.ResponseWriter, r *http.Request) {
+			f, _ := validBoth(r, formID)
+			page(w, r, f)
+		}))
+}
+
+//Start listening to each websocket client that connects.
+func processSocket(ws *websocket.Conn) {
+	var msg string
+	var formID uint8
+	var err error
+	send := func(str string) {
+		if err = websocket.Message.Send(ws, str); err != nil {
+			warn.Println(err)
+		}
+	}
+	//Start a loop to listen to incoming websocket traffic from all clients.
+	for {
+		//Ignore any empty messages.
+		if websocket.Message.Receive(ws, &msg) != nil || len(msg) < 1 {
+			break
+		}
+		//The first character of the websocket message is used as a "router" to decide where to send the message.
+		formID = msg[0]
+		//Ignore any messages that do not have a case in this switch.
+		switch formID {
+		case eventTotalScores:
+			var form url.Values
+			err = json.Unmarshal([]byte(msg[1:]), &form)
+			if err != nil {
+				warn.Println(err)
+				continue
+			}
+			if form, passed := isValid(form, formID); passed {
+				send(eventTotalUpsert(form.Fields))
+			} else {
+				send(fmt.Sprintf("Unable to save %v.", msg))
+			}
+		case eventUpdateShotScore:
+			var form url.Values
+			err = json.Unmarshal([]byte(msg[1:]), &form)
+			if err != nil {
+				warn.Println(err)
+				continue
+			}
+
+			if form, passed := isValid(form, formID); passed {
+				send("!" + updateShotScores(form.Fields))
+			} else {
+				var response []byte
+				response, err = json.Marshal(form)
+				if err != nil {
+					warn.Println(err)
+					continue
+				}
+				send(fmt.Sprintf("!%U%s", msg[0], response))
+			}
+		case 126: //getDisciplines:
+			var response []byte
+			response, err = json.Marshal(globalDisciplines)
+			if err != nil {
+				warn.Println(err)
+				continue
+			}
+			send(fmt.Sprintf("~%s", response))
+		}
+	}
+}
